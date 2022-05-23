b37efff71eacecc33fe5369f6b793f5e02f1088a
Cary hu <cary.hu@grapecity.com>
2022-05-23 20:08:46 +0800

    20220523

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -6,6 +6,115 @@ var a = new Solution();
 a.TwoSum(new int[] { 5, 25, 75 }, 100);
 public class Solution
 {
+    public void ReverseString(char[] s)
+    {
+        var i = 0;
+        var j = s.Length - 1;
+        while (i < j)
+        {
+            var temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public string ReverseWords(string s)
+    {
+        var words = s.Split(' ');
+        var result = new string[words.Length];
+        for (int i = 0; i < words.Length; i++)
+        {
+            var word = words[i];
+            var tempWord = "";
+            for (var j = word.Length - 1; j >= 0; j--)
+            {
+                tempWord += word[j];
+            }
+            result[i] = tempWord;
+        }
+        return string.Join(" ", result);
+
+    }
+    int[][] dirs = { new int[] { -1, 0 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { 0, 1 } };
+
+    public int CutOffTree(IList<IList<int>> forest)
+    {
+        List<Tuple<int, int>> trees = new List<Tuple<int, int>>();
+        int row = forest.Count;
+        int col = forest[0].Count;
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                if (forest[i][j] > 1)
+                {
+                    trees.Add(new Tuple<int, int>(i, j));
+                }
+            }
+        }
+        trees.Sort((a, b) => forest[a.Item1][a.Item2] - forest[b.Item1][b.Item2]);
+
+        int cx = 0;
+        int cy = 0;
+        int ans = 0;
+        for (int i = 0; i < trees.Count; ++i)
+        {
+            int steps = BFS(forest, cx, cy, trees[i].Item1, trees[i].Item2);
+            if (steps == -1)
+            {
+                return -1;
+            }
+            ans += steps;
+            cx = trees[i].Item1;
+            cy = trees[i].Item2;
+        }
+        return ans;
+    }
+
+    public int BFS(IList<IList<int>> forest, int sx, int sy, int tx, int ty)
+    {
+        if (sx == tx && sy == ty)
+        {
+            return 0;
+        }
+
+        int row = forest.Count;
+        int col = forest[0].Count;
+        int step = 0;
+        Queue<Tuple<int, int>> queue = new Queue<Tuple<int, int>>();
+        bool[,] visited = new bool[row, col];
+        queue.Enqueue(new Tuple<int, int>(sx, sy));
+        visited[sx, sy] = true;
+        while (queue.Count > 0)
+        {
+            step++;
+            int sz = queue.Count;
+            for (int i = 0; i < sz; ++i)
+            {
+                Tuple<int, int> cell = queue.Dequeue();
+                int cx = cell.Item1, cy = cell.Item2;
+                for (int j = 0; j < 4; ++j)
+                {
+                    int nx = cx + dirs[j][0];
+                    int ny = cy + dirs[j][1];
+                    if (nx >= 0 && nx < row && ny >= 0 && ny < col)
+                    {
+                        if (!visited[nx, ny] && forest[nx][ny] > 0)
+                        {
+                            if (nx == tx && ny == ty)
+                            {
+                                return step;
+                            }
+                            queue.Enqueue(new Tuple<int, int>(nx, ny));
+                            visited[nx, ny] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
     public int[] TwoSum(int[] numbers, int target)
     {
         int[] result = new int[2];
