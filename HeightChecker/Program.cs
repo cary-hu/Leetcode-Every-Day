a7fc310ca0c136b648fd33e678b7db52c6b05dcc
Cary hu <cary.hu@grapecity.com>
2022-06-13 19:40:39 +0800

    20220613

---
+++ HeightChecker/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20220613
+/// https://leetcode.cn/problems/height-checker/
+/// </summary>
+public class Solution
+{
+    public int HeightChecker(int[] heights)
+    {
+        var res = 0;
+        var sorted = heights.ToList().OrderBy(x => x).ToArray();
+        
+        for (int i = 0; i < heights.Length; i++)
+        {
+            if (heights[i] != sorted[i])
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
