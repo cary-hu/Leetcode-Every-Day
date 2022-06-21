7b8191495b1b5abcf6474b45cc94d3eae29bbf90
Cary Hu <cary.hu@grapecity.com>
2022-06-21 08:38:47 +0800

    20220621

---
+++ DefangIPaddr/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 20220621
+/// https://leetcode.cn/problems/defanging-an-ip-address/
+/// </summary>
+public class Solution
+{
+    public string DefangIPaddr(string address)
+    {
+        return address.Replace(".", "[.]");
+    }
+}
