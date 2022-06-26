57e11719d1c74de5f2190174b2c20496384cf862
Cary hu <cary.hu@grapecity.com>
2022-06-26 09:38:56 +0800

    20220626

---
+++ RandomPickWithBlacklist/RandomPickWithBlacklist/Program.cs
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// 20220626
+/// https://leetcode.cn/problems/random-pick-with-blacklist/
+/// </summary>
+public class Solution
+{
+    Dictionary<int, int> b2w = new Dictionary<int, int>();
+    Random random = new Random();
+    int bound;
+
+    public Solution(int n, int[] blacklist)
+    {
+        int m = blacklist.Length;
+        bound = n - m;
+        ISet<int> black = new HashSet<int>();
+        foreach (int b in blacklist)
+        {
+            if (b >= bound)
+            {
+                black.Add(b);
+            }
+        }
+
+        int w = bound;
+        foreach (int b in blacklist)
+        {
+            if (b < bound)
+            {
+                while (black.Contains(w))
+                {
+                    ++w;
+                }
+                b2w.Add(b, w);
+                ++w;
+            }
+        }
+    }
+
+    public int Pick()
+    {
+        int x = random.Next(bound);
+        return b2w.ContainsKey(x) ? b2w[x] : x;
+    }
+}
