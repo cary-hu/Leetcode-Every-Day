dfd11d5fa9939dff3a7eb6d00243a833d3b37582
Cary hu <cary.hu@grapecity.com>
2022-07-02 11:24:49 +0800

    20220702

---
+++ MinRefuelStops/MinRefuelStops/Program.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// 20220702
+/// https://leetcode.cn/problems/minimum-number-of-refueling-stops/
+/// </summary>
+public class Solution
+{
+    public int MinRefuelStops(int target, int startFuel, int[][] stations)
+    {
+        int n = stations.Length;
+        long[] dp = new long[n + 1];
+        dp[0] = startFuel;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                if (dp[j] >= stations[i][0])
+                {
+                    dp[j + 1] = Math.Max(dp[j + 1], dp[j] + stations[i][1]);
+                }
+            }
+        }
+        for (int i = 0; i <= n; i++)
+        {
+            if (dp[i] >= target)
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
