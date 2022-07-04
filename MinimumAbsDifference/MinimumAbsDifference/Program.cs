f6ddd5e969aa190a3ce8877ccb3b488e1a978738
Cary Hu <cary.hu@grapecity.com>
2022-07-04 08:37:41 +0800

    20220704

---
+++ MinimumAbsDifference/MinimumAbsDifference/Program.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// 20220407
+/// https://leetcode.cn/problems/minimum-absolute-difference/
+/// </summary>
+public class Solution
+{
+    public IList<IList<int>> MinimumAbsDifference(int[] arr)
+    {
+        Array.Sort(arr);
+        int min = int.MaxValue;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i + 1 >= arr.Length)
+            {
+                break;
+            }
+            min = Math.Min(min, Math.Abs(arr[i] - arr[i + 1]));
+        }
+        var res = new List<IList<int>>();
+        for (int i = 0; i < arr.Length;)
+        {
+            if (i + 1 >= arr.Length)
+            {
+                break;
+            }
+            if (Math.Abs(arr[i] - arr[i + 1]) == min)
+            {
+                res.Add(new List<int>()
+                {
+                    arr[i++],
+                    arr[i]
+                });
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return res;
+    }
+}
