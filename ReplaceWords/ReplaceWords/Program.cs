eab6b06863adb9f32a2197111222ec3222e35c28
Cary Hu <cary.hu@grapecity.com>
2022-07-07 17:02:53 +0800

    20220707

---
+++ ReplaceWords/ReplaceWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+/// <summary>
+/// 20220707
+/// https://leetcode.cn/problems/replace-words/
+/// </summary>
+public class Solution
+{
+    public string ReplaceWords(IList<string> dictionary, string sentence)
+    {
+        var words = sentence.Split(' ');
+        var result = new StringBuilder();
+        dictionary = dictionary.OrderBy(x => x.Length).ToList();
+        var res = "";
+        foreach (var item in words)
+        {
+            var root = dictionary.Where(x => item.StartsWith(x)).FirstOrDefault();
+            if(!string.IsNullOrEmpty(root))
+            {
+                res = root;
+            }
+            else
+            {
+                res = item;
+            }
+            result.Append(res + " ");
+        }
+        return result.ToString().Trim();
+    }
+}
