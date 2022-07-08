285c9f192c061c7adb8b6d1a0e017c9c10bba505
Cary Hu <cary.hu@grapecity.com>
2022-07-08 13:42:11 +0800

    20220708

---
+++ MinCostToMoveChips/MinCostToMoveChips/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 20220708
+/// https://leetcode.cn/problems/minimum-cost-to-move-chips-to-the-same-position/
+/// </summary>
+public class Solution
+{
+    public int MinCostToMoveChips(int[] position)
+    {
+        int even = 0;
+        foreach (var p in position)
+        {
+            if (p % 2 == 0)
+            {
+                even++;
+            }
+        }
+        return Math.Min(position.Count() - even, even);
+    }
+}
