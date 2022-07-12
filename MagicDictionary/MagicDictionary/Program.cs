3245245d4571f59180a97be5f2f438fe1de7f6c0
Cary Hu <cary.hu@grapecity.com>
2022-07-12 08:43:48 +0800

    20220712

---
+++ MagicDictionary/MagicDictionary/Program.cs
@@ -0,0 +1,55 @@
+﻿/// <summary>
+/// 20220711
+/// https://leetcode.cn/problems/implement-magic-dictionary/
+/// </summary>
+public class MagicDictionary
+{
+
+    private string[] words;
+
+    public MagicDictionary()
+    {
+
+    }
+
+    public void BuildDict(string[] dictionary)
+    {
+        words = dictionary;
+    }
+
+    public bool Search(string searchWord)
+    {
+        foreach (string word in words)
+        {
+            if (word.Length != searchWord.Length)
+            {
+                continue;
+            }
+
+            int diff = 0;
+            for (int i = 0; i < word.Length; ++i)
+            {
+                if (word[i] != searchWord[i])
+                {
+                    ++diff;
+                    if (diff > 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (diff == 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.BuildDict(dictionary);
+ * bool param_2 = obj.Search(searchWord);
+ */
---
+++ OddCells/OddCells/Program.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// 20220712
+/// https://leetcode.cn/problems/cells-with-odd-values-in-a-matrix/
+/// </summary>
+public class Solution
+{
+    public int OddCells(int m, int n, int[][] indices)
+    {
+        int[,] matrix = new int[m, n];
+        int res = 0;
+        foreach (var index in indices)
+        {
+            int i = index[0];
+            int j = index[1];
+            for (int k = 0; k < m; k++)
+            {
+                matrix[k, j]++;
+            }
+            for (int k = 0; k < n; k++)
+            {
+                matrix[i, k]++;
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i,j] % 2 != 0)
+                {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
