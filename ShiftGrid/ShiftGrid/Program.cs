c9970c02af3134a5fc831d2f392cde679940b2c9
Cary Hu <cary.hu@grapecity.com>
2022-07-20 08:45:28 +0800

    20220720

---
+++ ShiftGrid/ShiftGrid/Program.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// 20220720
+/// https://leetcode.cn/problems/shift-2d-grid/
+/// </summary>
+public class Solution
+{
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+    {
+        var list = new List<int>();
+        var row = grid.Length;
+        var col = grid[0].Length;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                list.Add(grid[i][j]);
+            }
+        }
+        var res = new List<IList<int>>();
+        var rowRes = new List<int>();
+        var index = list.Count - k % (row * col);
+        for (int i = index; i < list.Count; i++)
+        {
+            rowRes.Add(list[i]);
+            if (rowRes.Count == col)
+            {
+                res.Add(rowRes);
+                rowRes = new List<int>();
+            }
+        }
+        for (int i = 0; i < index; i++)
+        {
+            rowRes.Add(list[i]);
+            if (rowRes.Count == col)
+            {
+                res.Add(rowRes);
+                rowRes = new List<int>();
+            }
+        }
+        return res;
+    }
+}
