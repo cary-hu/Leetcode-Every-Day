f68b945a845a3d099b2b8f8183e11be5515b84c4
Cary hu <cary.hu@grapecity.com>
2022-07-24 12:40:14 +0800

    20220724

---
+++ DistanceBetweenBusStops/DistanceBetweenBusStops/Program.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// 20220724
+/// https://leetcode.cn/problems/distance-between-bus-stops/
+/// </summary>
+public class Solution
+{
+    public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+    {
+        if (start > destination)
+        {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < distance.Length; i++)
+        {
+            if (i >= start && i < destination)
+            {
+                sum1 += distance[i];
+            }
+            else
+            {
+                sum2 += distance[i];
+            }
+        }
+        return Math.Min(sum1, sum2);
+    }
+}
