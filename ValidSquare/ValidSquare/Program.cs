db6fb9583c1726b040e68094d6ae21e5ddd88c02
Cary Hu <cary.hu@grapecity.com>
2022-07-29 13:42:32 +0800

    20220729

---
+++ ValidSquare/ValidSquare/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20220729
+/// https://leetcode.cn/problems/valid-square/
+/// </summary>
+public class Solution
+{
+    public static bool ValidSquare(int[] p1, int[] p2, int[] p3, int[] p4)
+    {
+        var list = new List<int>
+        {
+            GetDis(p1, p2),
+            GetDis(p1, p3),
+            GetDis(p1, p4),
+            GetDis(p2, p3),
+            GetDis(p2, p4),
+            GetDis(p3, p4)
+        };
+        list = list.Distinct().ToList();
+        return list.Count == 2 && (list[0] != 0 && list[1] != 0);
+    }
+    private static int GetDis(int[] p1, int[] p2)
+    {
+        return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
+    }
+}
