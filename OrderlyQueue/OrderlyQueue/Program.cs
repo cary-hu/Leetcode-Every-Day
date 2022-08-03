1babf09ce85a2758b3c89b7e5ad97a11cbb83c55
Cary Hu <cary.hu@grapecity.com>
2022-08-03 08:55:41 +0800

    20220803

---
+++ OrderlyQueue/OrderlyQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// 20220803
+/// https://leetcode.cn/problems/orderly-queue/
+/// </summary>
+public class Solution
+{
+    public string OrderlyQueue(string s, int k)
+    {
+        if (k == 1)
+        {
+            var temp = s + s;
+            var min = s;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var currentString = temp.Substring(i, s.Length);
+                if (min.CompareTo(currentString) > 0)
+                {
+                    min = currentString;
+                }
+            }
+            return min;
+        }
+        else
+        {
+            return string.Join("", s.OrderBy(x => x).ToList());
+        }
+
+    }
+}
