f5b56ef683f9602e27a227e5a3b8f9f5d17e5bf4
Cary Hu <cary.hu@grapecity.com>
2022-08-04 08:51:26 +0800

    20220804

---
+++ MinSubsequence/MinSubsequence/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 20220804
+/// https://leetcode.cn/problems/minimum-subsequence-in-non-increasing-order/
+/// </summary>
+public class Solution
+{
+    public IList<int> MinSubsequence(int[] nums)
+    {
+        var res = new List<int>();
+        var tempNum = nums.ToList().OrderByDescending(x => x).ToList();
+        var total = tempNum.Sum();
+        for (int i = 0; i < tempNum.Count; i++)
+        {
+            res.Add(tempNum[i]);
+            var resSum = res.Sum();
+            
+            if (total - resSum < resSum)
+            {
+                break;
+            }
+        }
+        return res;
+    }
+}
