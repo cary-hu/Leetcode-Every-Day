0d72b27baf276ba6e88ed62f47884d978aa3207e
Cary hu <cary.hu@grapecity.com>
2022-08-07 14:08:35 +0800

    20220807

---
+++ pushcode.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd "E:\Code\Leetcode-Every-Day"
+echo -e  "
+▶ \033[33;1mgit add .
+\033[0m"
+git add .
+
+git status
+echo -e "
+▶ \033[33;1mcommit message:
+\033[37;1m"
+read msg
+if [ ! $msg ]; then
+    msg=$(date '+%Y%m%d')
+fi
+echo "Use default commit message: $msg"
+echo -e "
+▶ \033[33;1mgit commit -m '$msg'
+\033[0m"
+git commit -m "$msg"
+
+# echo -e "
+# ▶ \033[33;1mgit push
+# "
+# echo -e "\033[37;1mstart pushing ...\033[0m
+# "
+# git push
+# echo -e "
+# \033[37;1mAll Done\033[0m"
