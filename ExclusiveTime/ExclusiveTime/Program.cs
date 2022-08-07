7a5b66e6e0efaf127b704b6a0012446a290c2901
Cary hu <cary.hu@grapecity.com>
2022-08-07 13:52:15 +0800

    20220807

---
+++ ExclusiveTime/ExclusiveTime/Program.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// 20220807
+/// https://leetcode.cn/problems/exclusive-time-of-functions/
+/// </summary>
+public class Solution
+{
+    public int[] ExclusiveTime(int n, IList<string> logs)
+    {
+        var stack = new Stack<(int functionId, int timeStamp, string action, int tempEnd)>();
+        int[] res = new int[n];
+        foreach (var log in logs)
+        {
+            var logParts = log.Split(":");
+            var functionId = int.Parse(logParts[0]);
+            var action = logParts[1];
+            var timeStamp = int.Parse(logParts[2]);
+            if (action == "start")
+            {
+                stack.Push((functionId, timeStamp, action, 0));
+            }
+            if (action == "end")
+            {
+                var stackHead = stack.Pop();
+                int interval = timeStamp - stackHead.timeStamp + 1;
+                res[functionId] += interval;
+                if(stack.Count > 0)
+                {
+                    res[stack.Peek().functionId] -= interval;
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ StringMatching/StringMatching/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20220806
+/// https://leetcode.cn/problems/string-matching-in-an-array/
+/// </summary>
+public class Solution
+{
+    public IList<string> StringMatching(string[] words)
+    {
+        var res = new List<string>();
+        foreach (var word in words)
+        {
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (word == words[j] || words[j].Length > word.Length)
+                {
+                    continue;
+                }
+                if (word.Contains(words[j]))
+                {
+                    res.Add(words[j]);
+                }
+            }
+        }
+        return res.Distinct().ToList();
+    }
+}
