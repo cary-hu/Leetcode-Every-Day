56cb51d8d6f3e52af23de83fced1033842cfd9e7
Cary Hu <cary.hu@grapecity.com>
2022-08-09 08:51:16 +0800

    20220809

---
+++ MinStartValue/MinStartValue/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20220809
+/// https://leetcode.cn/problems/minimum-value-to-get-positive-step-by-step-sum/
+/// </summary>
+public class Solution
+{
+    public int MinStartValue(int[] nums)
+    {
+        int tmp = nums[0], sum = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            tmp = Math.Min(tmp, sum);
+        }
+        return Math.Max(1, 1 - tmp);
+    }
+}
