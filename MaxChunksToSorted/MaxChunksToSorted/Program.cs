2742e7fbdde4f2f5573613d8fac72d092f7ab850
Cary hu <cary.hu@grapecity.com>
2022-08-13 10:32:42 +0800

    20220813

---
+++ MaxChunksToSorted/MaxChunksToSorted/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20220813
+/// https://leetcode.cn/problems/max-chunks-to-make-sorted-ii/
+/// </summary>
+public class Solution
+{
+    public int MaxChunksToSorted(int[] arr)
+    {
+        var stack = new Stack<int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int element = arr[i];
+            if (stack.Count > 0)
+            {
+                element = Math.Max(arr[i], stack.Peek());
+            }
+            while(stack.Count > 0 && arr[i] < stack.Peek())
+            {
+                stack.Pop();
+            }
+            stack.Push(element);
+        }
+        return stack.Count;
+    }
+}
