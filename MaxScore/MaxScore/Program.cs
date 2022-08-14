4fde67b5134f77af7cb683629adf5275f0974e85
Cary hu <cary.hu@grapecity.com>
2022-08-14 12:30:50 +0800

    20220814

---
+++ MaxScore/MaxScore/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 20220814
+/// https://leetcode.cn/problems/maximum-score-after-splitting-a-string/
+/// </summary>
+public class Solution
+{
+    public int MaxScore(string s)
+    {
+        var maxNum = int.MinValue;
+        for (int i = 1; i < s.Length; i++)
+        {
+            maxNum = Math.Max(maxNum, GetZeroNumber(s[..i]) + GetOneNumber(s[i..]));
+        }
+        return maxNum;
+    }
+    private int GetZeroNumber(string s)
+    {
+        return s.Where(x => x == '0').Count();
+    }
+    private int GetOneNumber(string s)
+    {
+        return s.Where(x => x == '1').Count();
+    }
+}
