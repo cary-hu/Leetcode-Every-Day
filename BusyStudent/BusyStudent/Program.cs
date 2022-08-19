c75dfe964ba8aefb2336fa2d23dbc06e07307c55
Cary Hu <cary.hu@grapecity.com>
2022-08-19 08:40:06 +0800

    20220819

---
+++ BusyStudent/BusyStudent/Program.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// 20220819
+/// https://leetcode.cn/problems/number-of-students-doing-homework-at-a-given-time/
+/// </summary>
+public class Solution
+{
+    public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+    {
+        var res = 0;
+        var studentCount = startTime.Length;
+        for (int i = 0; i < studentCount; i++)
+        {
+            if(queryTime >= startTime[i] && queryTime <= endTime[i])
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
