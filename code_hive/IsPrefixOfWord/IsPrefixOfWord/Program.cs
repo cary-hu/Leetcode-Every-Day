106fae051cf3eb37cc740eb9b196ab2cb54fe901
Cary hu <cary.hu@grapecity.com>
2022-08-21 10:14:31 +0800

    20220821

---
+++ code_hive/IsPrefixOfWord/IsPrefixOfWord/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20220821
+/// https://leetcode.cn/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+/// </summary>
+public class Solution
+{
+    public int IsPrefixOfWord(string sentence, string searchWord)
+    {
+        var words = sentence.Split(" ");
+        var res = -1;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].StartsWith(searchWord))
+            {
+                res = i + 1;
+                break;
+            }
+        }
+        return res;
+    }
+}
