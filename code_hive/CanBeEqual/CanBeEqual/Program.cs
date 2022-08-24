2635b56f9b080a584cd6cc8fbaf535afc1b4131e
Cary Hu <cary.hu@grapecity.com>
2022-08-24 09:57:07 +0800

    20220824

---
+++ code_hive/CanBeEqual/CanBeEqual/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20220824
+/// https://leetcode.cn/problems/make-two-arrays-equal-by-reversing-sub-arrays/
+/// </summary>
+public class Solution
+{
+    public bool CanBeEqual(int[] target, int[] arr)
+    {
+        if (target.Length != arr.Length)
+        {
+            return false;
+        }
+        Array.Sort(target);
+        Array.Sort(arr);
+        return target.SequenceEqual(arr);
+    }
+}
