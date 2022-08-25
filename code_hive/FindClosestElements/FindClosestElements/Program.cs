5042f857199873e559e62beffe152511a76e44b2
Cary Hu <cary.hu@grapecity.com>
2022-08-25 09:31:59 +0800

    20220825

---
+++ code_hive/FindClosestElements/FindClosestElements/Program.cs
@@ -0,0 +1,31 @@
+﻿var a = new Solution();
+a.FindClosestElements(new int[] { -2, -1, 1, 2, 3, 4, 5 }, 7, 3);
+/// <summary>
+/// 20220825
+/// https://leetcode.cn/problems/find-k-closest-elements/
+/// </summary>
+public class Solution
+{
+    public IList<int> FindClosestElements(int[] arr, int k, int x)
+    {
+        Array.Sort(arr, (a, b) =>
+        {
+            if (Math.Abs(a - x) != Math.Abs(b - x))
+            {
+                return Math.Abs(a - x) - Math.Abs(b - x);
+            }
+            else
+            {
+                return a - b;
+            }
+        });
+        int[] closest = arr.Take(k).ToArray();
+        Array.Sort(closest);
+        IList<int> ans = new List<int>();
+        foreach (int num in closest)
+        {
+            ans.Add(num);
+        }
+        return ans;
+    }
+}
