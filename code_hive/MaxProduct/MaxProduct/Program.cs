a473c84456495577658156356cfc78bd2469112d
Cary Hu <cary.hu@grapecity.com>
2022-08-26 08:45:10 +0800

    20220826

---
+++ code_hive/MaxProduct/MaxProduct/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 20220826
+/// https://leetcode.cn/problems/maximum-product-of-two-elements-in-an-array/
+/// </summary>
+public class Solution
+{
+    public int MaxProduct(int[] nums)
+    {
+        var tempNum = nums.OrderByDescending(x => x).Take(2).ToArray();
+
+        return (tempNum[0] - 1) * (tempNum[1] - 1);
+    }
+}
