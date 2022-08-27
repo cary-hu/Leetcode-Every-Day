6efde057a8210092256bf4ad12e2632f9a92ac49
Cary hu <cary.hu@grapecity.com>
2022-08-27 10:18:54 +0800

    20220827

---
+++ code_hive/WidthOfBinaryTree/WidthOfBinaryTree/Program.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// 20220827
+/// https://leetcode.cn/problems/maximum-width-of-binary-tree/
+/// </summary>
+public class Solution
+{
+    Dictionary<int, int> levelMin = new Dictionary<int, int>();
+
+    public int WidthOfBinaryTree(TreeNode root)
+    {
+        return DFS(root, 1, 1);
+    }
+
+    public int DFS(TreeNode node, int depth, int index)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+        levelMin.TryAdd(depth, index); // 每一层最先访问到的节点会是最左边的节点，即每一层编号的最小值
+        return Math.Max(index - levelMin[depth] + 1, Math.Max(DFS(node.left, depth + 1, index * 2), DFS(node.right, depth + 1, index * 2 + 1)));
+    }
+}
+
+
+// Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ code_hive/WidthOfBinaryTree/WidthOfBinaryTree/Solution2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidthOfBinaryTree
+{
+    public class Solution2
+    {
+        public int WidthOfBinaryTree(TreeNode root)
+        {
+            int res = 1;
+            IList<Tuple<TreeNode, int>> arr = new List<Tuple<TreeNode, int>>();
+            arr.Add(new Tuple<TreeNode, int>(root, 1));
+            while (arr.Count > 0)
+            {
+                IList<Tuple<TreeNode, int>> tmp = new List<Tuple<TreeNode, int>>();
+                foreach (Tuple<TreeNode, int> pair in arr)
+                {
+                    TreeNode node = pair.Item1;
+                    int index = pair.Item2;
+                    if (node.left != null)
+                    {
+                        tmp.Add(new Tuple<TreeNode, int>(node.left, index * 2));
+                    }
+                    if (node.right != null)
+                    {
+                        tmp.Add(new Tuple<TreeNode, int>(node.right, index * 2 + 1));
+                    }
+                }
+                res = Math.Max(res, arr[arr.Count - 1].Item2 - arr[0].Item2 + 1);
+                arr = tmp;
+            }
+            return res;
+        }
+    }
+}
