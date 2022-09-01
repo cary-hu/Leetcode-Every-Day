15d2f54ed05fe9b584054309792ef923eeab9c21
Cary Hu <cary.hu@grapecity.com>
2022-09-01 08:42:48 +0800

    20220901

---
+++ code_hive/FinalPrices/FinalPrices/Program.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// 20220901
+/// https://leetcode.cn/problems/final-prices-with-a-special-discount-in-a-shop/
+/// </summary>
+public class Solution
+{
+    public int[] FinalPrices(int[] prices)
+    {
+        var res = new int[prices.Length];
+        for (int i = 0; i < prices.Length; i++)
+        {
+            var current = prices[i];
+            var minBounds = int.MaxValue;
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                if (prices[j] <= current)
+                {
+                    minBounds = prices[j];
+                    break;  
+                }
+            }
+            if(minBounds == int.MaxValue)
+            {
+                res[i] = current;
+            } else
+            {
+                res[i] = current - minBounds;
+            }
+        }
+        return res;
+    }
+}
