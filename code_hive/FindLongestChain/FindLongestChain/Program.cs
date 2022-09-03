310c8ac9627fcd27ac53a9ba29a3b0374f1b9af0
Cary hu <cary.hu@grapecity.com>
2022-09-03 21:06:14 +0800

    20220903

---
+++ code_hive/FindLongestChain/FindLongestChain/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 20220903
+/// https://leetcode.cn/problems/maximum-length-of-pair-chain/
+/// </summary>
+public class Solution
+{
+    public int FindLongestChain(int[][] pairs)
+    {
+        Array.Sort(pairs, (a, b) => { return a[1] - b[1]; });
+
+        var cur = int.MinValue;
+        var res = 0;
+        foreach (var pair in pairs)
+        {
+            if(cur < pair[0])
+            {
+                cur = pair[1];
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
