328936da8174bf40953d0a95cbdfa22b23e5d675
Cary Hu <cary.hu@grapecity.com>
2022-09-06 10:56:38 +0800

    20220906

---
+++ code_hive/Leetcode Algorithmic Basics/Day2/FindMin/FindMin/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 20220906
+/// https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array/
+/// </summary>
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        var l = 0;
+        var r = nums.Length;
+        while(l < r)
+        {
+            var mid = (l + r) >> 1;
+            if (nums[mid] <= nums.Last())
+            {
+                r = mid;
+            } else
+            {
+                l = mid + 1;
+            }
+        }
+        return nums[l];
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day2/FindPeakElement/FindPeakElement/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 20220906
+/// https://leetcode.cn/problems/find-peak-element/
+/// </summary>
+public class Solution
+{
+    public int FindPeakElement(int[] nums)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+        while( l < r)
+        {
+            var mid = (l + r) >> 1;
+            if (nums[mid] > nums[mid + 1])
+            {
+                r = mid;
+            } else
+            {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
