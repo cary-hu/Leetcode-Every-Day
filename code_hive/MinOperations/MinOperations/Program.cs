501ef01744d20d73c67b4c00a4ef52bd81d4a77b
Cary Hu <cary.hu@grapecity.com>
2022-09-09 08:33:41 +0800

    20220909

---
+++ code_hive/MinOperations/MinOperations/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20220909
+/// https://leetcode.cn/problems/crawler-log-folder/
+/// </summary>
+public class Solution
+{
+    public int MinOperations(string[] logs)
+    {
+        var deep = 0;
+        foreach (var log in logs)
+        {
+            if (log == "../")
+            {
+                if (deep > 0)
+                {
+                    deep--;
+                }
+            }
+            else if (log == "./")
+            {
+                continue;
+            }
+            else
+            {
+                deep++;
+            }
+        }
+        return deep;
+    }
+}
