16b28dfc7914c1ab3fb750c58912908eba3daca7
Cary Hu <cary.hu@grapecity.com>
2022-09-09 10:49:27 +0800

    20220909

---
+++ code_hive/Leetcode Algorithmic Basics/Day5/NumSubarrayProductLessThanK/NumSubarrayProductLessThanK/Program.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// 20220909
+/// https://leetcode.cn/problems/subarray-product-less-than-k/
+/// </summary>
+public class Solution
+{
+    public int NumSubarrayProductLessThanK(int[] nums, int k)
+    {
+        int n = nums.Length, res = 0;
+        int prod = 1, i = 0;
+        for (int j = 0; j < n; j++)
+        {
+            prod *= nums[j];
+            while (i <= j && prod >= k)
+            {
+                prod /= nums[i];
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+}
