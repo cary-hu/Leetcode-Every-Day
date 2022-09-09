952af1b16523eee4ca2115b806112ec9b2192e39
Cary Hu <cary.hu@grapecity.com>
2022-09-09 14:38:20 +0800

    20220909

---
+++ code_hive/Leetcode Algorithmic Basics/Day5/MinSubArrayLen/MinSubArrayLen/Program.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// 20220909
+/// https://leetcode.cn/problems/minimum-size-subarray-sum/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        var res = int.MaxValue;
+        var start = 0;
+        var end = 0;
+        var sum = 0;
+        while (end < nums.Length)
+        {
+            sum += nums[end];
+            while(sum >= target)
+            {
+                res = Math.Min(res, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+       
+        if (res == int.MaxValue)
+        {
+            return 0;
+        }
+        return res;
+    }
+}
