0cc8f4cbe3b76f78131c1f7dcd18927822bbd2c4
Cary Hu <cary.hu@grapecity.com>
2022-09-11 08:08:06 +0800

    20220911

---
+++ code_hive/MincostToHireWorkers/MincostToHireWorkers/Program.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// 20220911
+/// https://leetcode.cn/problems/minimum-cost-to-hire-k-workers/
+/// </summary>
+public class Solution
+{
+    public double MincostToHireWorkers(int[] quality, int[] wage, int k)
+    {
+        int n = quality.Length;
+        int[] h = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            h[i] = i;
+        }
+        Array.Sort(h, (a, b) => {
+            return quality[b] * wage[a] - quality[a] * wage[b];
+        });
+        double res = 1e9;
+        double totalq = 0.0;
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+        for (int i = 0; i < k - 1; i++)
+        {
+            totalq += quality[h[i]];
+            pq.Enqueue(quality[h[i]], -quality[h[i]]);
+        }
+        for (int i = k - 1; i < n; i++)
+        {
+            int idx = h[i];
+            totalq += quality[idx];
+            pq.Enqueue(quality[idx], -quality[idx]);
+            double totalc = ((double)wage[idx] / quality[idx]) * totalq;
+            res = Math.Min(res, totalc);
+            totalq -= pq.Dequeue();
+        }
+        return res;
+    }
+}
