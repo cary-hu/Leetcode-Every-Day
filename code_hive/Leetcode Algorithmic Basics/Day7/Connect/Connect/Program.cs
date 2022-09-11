da06a1e46623d6c3daf6bf280e3ac5e34fffb3ee
Cary Hu <cary.hu@grapecity.com>
2022-09-11 08:22:03 +0800

    20220911

---
+++ code_hive/Leetcode Algorithmic Basics/Day7/Connect/Connect/Program.cs
@@ -0,0 +1,67 @@
+﻿/// <summary>
+/// 20220911
+/// https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public Node Connect(Node root)
+    {
+        if (root == null)
+        {
+            return root;
+        }
+        var queue = new Queue<Node>();
+        queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            var currentLevel = new List<Node>();
+            while (queue.Count > 0)
+            {
+                currentLevel.Add(queue.Dequeue());
+            }
+            foreach (var item in currentLevel)
+            {
+                if (item.left != null)
+                {
+                    queue.Enqueue(item.left);
+                }
+                if (item.right != null)
+                {
+                    queue.Enqueue(item.right);
+                }
+            }
+            for (int i = 0; i < currentLevel.Count - 1; i++)
+            {
+                currentLevel[i].next = currentLevel[i + 1];
+            }
+            currentLevel[currentLevel.Count - 1].next = null;
+        }
+
+        return root;
+    }
+}
+
+
+// Definition for a Node.
+public class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() { }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next)
+    {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day7/IsSubtree/IsSubtree/Program.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+/// 20220911
+/// https://leetcode.cn/problems/subtree-of-another-tree/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    {
+        return DFS(root, subRoot);
+    }
+    private bool DFS(TreeNode root, TreeNode subRoot)
+    {
+        if (root == null)
+        {
+            return false;
+        }
+        return Check(root, subRoot) || DFS(root.left, subRoot) || DFS(root.right, subRoot);
+    }
+    private bool Check(TreeNode root, TreeNode subRoot)
+    {
+        if (root == null && subRoot == null)
+        {
+            return true;
+        }
+        if(root == null || subRoot == null || subRoot.val != root.val)
+        {
+            return false;
+        }
+        return Check(root.left, subRoot.left) && Check(root.right, subRoot.right);
+    }
+}
+
+// Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
