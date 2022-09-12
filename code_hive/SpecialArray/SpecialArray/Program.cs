5bf8e330026ab160ee398832aa18f7e5c1b6a348
Cary Hu <cary.hu@grapecity.com>
2022-09-12 10:03:47 +0800

    20220912

---
+++ code_hive/SpecialArray/SpecialArray/Program.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// 20220912
+/// https://leetcode.cn/problems/special-array-with-x-elements-greater-than-or-equal-x/
+/// </summary>
+public class Solution
+{
+    public int SpecialArray(int[] nums)
+    {
+        Array.Sort(nums);
+        var res = -1;
+        for (int i = 0; i < nums[^1] + 1; i++)
+        {
+            if (nums.Where(x => x >= i).Count() == i)
+            {
+                res = i;
+            }
+        }
+        return res;
+    }
+}
