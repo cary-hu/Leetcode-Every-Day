ad17e2342132db99b6369ec80aa343aadcad315f
Cary Hu <cary.hu@grapecity.com>
2022-09-13 08:45:42 +0800

    20220913

---
+++ code_hive/MaximumSwap/MaximumSwap/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20220913
+/// https://leetcode.cn/problems/maximum-swap/
+/// </summary>
+public class Solution
+{
+    public int MaximumSwap(int num)
+    {
+        char[] charArray = num.ToString().ToCharArray();
+        int n = charArray.Length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                Swap(charArray, i, j);
+                maxNum = Math.Max(maxNum, int.Parse(new string(charArray)));
+                Swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public void Swap(char[] charArray, int i, int j)
+    {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+}
