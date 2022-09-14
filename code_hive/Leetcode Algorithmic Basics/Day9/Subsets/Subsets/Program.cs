68b3a0191defe670b5076ffd188535f6d534c146
Cary Hu <cary.hu@grapecity.com>
2022-09-14 09:30:20 +0800

    20220914

---
+++ code_hive/Leetcode Algorithmic Basics/Day9/Subsets/Subsets/Program.cs
@@ -0,0 +1,25 @@
+﻿var a = new Solution();
+a.Subsets(new int[] { 1, 2, 3 });
+/// <summary>
+/// 20220914
+/// https://leetcode.cn/problems/subsets/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+        var res = new List<IList<int>>();
+        BackTrack(nums, res, new List<int>(), 0);
+        return res;
+    }
+    private void BackTrack(int[] nums, List<IList<int>> res, List<int> path, int start)
+    {
+        res.Add(new List<int>(path));
+        for (int i = start; i < nums.Length; i++)
+        {
+            path.Add(nums[i]);
+            BackTrack(nums, res, path, i + 1);
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day9/SubsetsWithDup/SubsetsWithDup/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 20220914
+/// https://leetcode.cn/problems/subsets-ii/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public IList<IList<int>> SubsetsWithDup(int[] nums)
+    {
+        Array.Sort(nums);
+        var res = new List<IList<int>>();
+        BackTrack(nums, res, new List<int>(), 0);
+        return res;
+    }
+    private void BackTrack(int[] nums, List<IList<int>> res, List<int> path, int start)
+    {
+        res.Add(new List<int>(path));
+        for (int i = start; i < nums.Length; i++)
+        {
+            if (i > start && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            path.Add(nums[i]);
+            BackTrack(nums, res, path, i + 1);
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+}
