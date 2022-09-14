126ed629cf9812f98d16d0b3ce8ca0259fffae5c
Cary Hu <cary.hu@grapecity.com>
2022-09-14 08:44:46 +0800

    20220914

---
+++ code_hive/TrimMean/TrimMean/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 20220914
+/// https://leetcode.cn/problems/mean-of-array-after-removing-some-elements/
+/// </summary>
+public class Solution
+{
+    public double TrimMean(int[] arr)
+    {
+        Array.Sort(arr);
+        var n = arr.Length;
+        var sum = 0;
+        for (int i = (int)(n * 0.05); i < (int)(n * 0.95); i++)
+        {
+            sum += arr[i];
+        }
+        return sum / (n * 0.9);
+    }
+}
