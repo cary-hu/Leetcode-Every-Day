2aafd32c424112c94e65c9e30e271bde8587913b
Cary Hu <cary.hu@grapecity.com>
2022-09-15 08:36:02 +0800

    20220915

---
+++ code_hive/FlipLights/FlipLights/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20220915
+/// https://leetcode.cn/problems/bulb-switcher-ii/
+/// </summary>
+public class Solution
+{
+    public int FlipLights(int n, int presses)
+    {
+        if (presses == 0)
+        {
+            return 1;
+        }
+        if (n == 1)
+        {
+            return 2;
+        }
+        else if (n == 2)
+        {
+            return presses == 1 ? 3 : 4;
+        }
+        else
+        {
+            return presses == 1 ? 4 : presses == 2 ? 7 : 8;
+        }
+    }
+}
