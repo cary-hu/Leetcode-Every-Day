01b1e5cac750ceea6444cb800de576f5d591babf
Cary Hu <cary.hu@grapecity.com>
2022-09-16 09:17:36 +0800

    20220916

---
+++ code_hive/Leetcode Algorithmic Basics/Day12/CanJump/CanJump/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20220916
+/// https://leetcode.cn/problems/jump-game/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > k) return false;
+            k = Math.Max(k, i + nums[i]);
+        }
+        return true;
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day12/Rob/Rob/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20220916
+/// https://leetcode.cn/problems/house-robber-ii/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        int n = nums.Length;
+        if(n == 1)
+        {
+            return nums[0];
+        } else if(n == 2)
+        {
+            return Math.Max(nums[0], nums[1]);
+        }
+        return Math.Max(RobRange(nums, 0, n - 2), RobRange(nums, 1, n - 1));
+    }
+    private int RobRange(int[] nums, int start, int end)
+    {
+        int first = nums[start], second = Math.Max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++)
+        {
+            int temp = second;
+            second = Math.Max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+}
