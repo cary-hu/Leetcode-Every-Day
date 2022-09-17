7d1d078fc414b14534cb927db1a3bd21bc90eeee
Cary hu <cary.hu@grapecity.com>
2022-09-17 08:24:50 +0800

    20220917

---
+++ code_hive/Leetcode Algorithmic Basics/Day13/Jump/Jump/Program.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// 20220917
+/// https://leetcode.cn/problems/jump-game-ii/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int Jump(int[] nums)
+    {
+        int ans = 0;
+        int end = 0;
+        int maxPos = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            maxPos = Math.Max(nums[i] + i, maxPos);
+            if (i == end)
+            {
+                end = maxPos;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day13/UniquePaths/UniquePaths/Program.cs
@@ -0,0 +1,28 @@
+﻿new Solution().UniquePaths(3, 7);
+/// <summary>
+/// 20220917
+/// https://leetcode.cn/problems/unique-paths/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int UniquePaths(int m, int n)
+    {
+        var dp = new List<List<int>>();
+        for (int i = 0; i < m; i++)
+        {
+            dp.Add(new List<int> { 1 });
+        }
+        for (int j = 0; j < n; j++)
+        {
+            dp[0].Add(1);
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i].Add(dp[i - 1][j] + dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
