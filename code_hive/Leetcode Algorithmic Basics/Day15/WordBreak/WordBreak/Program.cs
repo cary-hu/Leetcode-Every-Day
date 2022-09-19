c01a4e177bb7250798bd8b761e2b4ef09feb9f0e
Cary Hu <cary.hu@grapecity.com>
2022-09-19 09:34:47 +0800

    20220919

--- code_hive/Leetcode Algorithmic Basics/Day15/WordBreak/WordBreak/Program.cs
+++ code_hive/Leetcode Algorithmic Basics/Day15/WordBreak/WordBreak/Program.cs
@@ -6,6 +6,20 @@ public class Solution
 {
     public bool WordBreak(string s, IList<string> wordDict)
     {
-
+        var wordDictSet = new HashSet<string>(wordDict);
+        var dp = new bool[s.Length + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (dp[j] && wordDictSet.Contains(s[j..i]))
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.Length];
     }
 }
