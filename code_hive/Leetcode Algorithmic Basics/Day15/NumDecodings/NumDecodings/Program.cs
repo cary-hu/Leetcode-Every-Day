dee0485bb8660e7a82c784cc0899703c140d98e1
Cary Hu <cary.hu@grapecity.com>
2022-09-19 08:56:08 +0800

    20220919

---
+++ code_hive/Leetcode Algorithmic Basics/Day15/NumDecodings/NumDecodings/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20220919
+/// https://leetcode.cn/problems/decode-ways/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int NumDecodings(string s)
+    {
+        int n = s.Length;
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            if (s[i - 1] != '0')
+            {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + (s[i - 1] - '0') <= 26))
+            {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day15/WordBreak/WordBreak/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 20220919
+/// https://leetcode.cn/problems/word-break/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+
+    }
+}
