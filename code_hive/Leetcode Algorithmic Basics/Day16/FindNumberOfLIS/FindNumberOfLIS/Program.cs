e45d2a8d4ab7c08d1f1d45fb6606ccdd48d6568b
Cary Hu <cary.hu@grapecity.com>
2022-09-20 08:56:57 +0800

    20220920

---
+++ code_hive/Leetcode Algorithmic Basics/Day16/FindNumberOfLIS/FindNumberOfLIS/Program.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+/// 20220920
+/// https://leetcode.cn/problems/number-of-longest-increasing-subsequence/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int FindNumberOfLIS(int[] nums)
+    {
+        int n = nums.Length, maxLen = 0, ans = 0;
+        int[] dp = new int[n];
+        Array.Fill(dp, 1);
+        int[] cnt = new int[n];
+        Array.Fill(cnt, 1);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[i] > nums[j])
+                {
+                    if (dp[j] + 1 > dp[i])
+                    {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    }
+                    else if (dp[j] + 1 == dp[i])
+                    {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            if (dp[i] > maxLen)
+            {
+                maxLen = dp[i];
+                ans = cnt[i];
+            }
+            else if (dp[i] == maxLen)
+            {
+                ans += cnt[i];
+            }
+        }
+        return ans;
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day16/LengthOfLIS/LengthOfLIS/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20220920
+/// https://leetcode.cn/problems/longest-increasing-subsequence/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int n = nums.Length;
+        var dp = new int[n];
+        Array.Fill(dp, 1);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res = Math.Max(res, dp[i]);
+        }
+        return res;
+    }
+}
