bc569f4c267bfc95a291ebcc14576028cc0c91dd
Cary Hu <cary.hu@grapecity.com>
2022-09-23 09:15:02 +0800

    20220923

---
+++ code_hive/Leetcode Algorithmic Basics/Day19/RangeBitwiseAnd/RangeBitwiseAnd/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 20220923
+/// https://leetcode.cn/problems/bitwise-and-of-numbers-range/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public int RangeBitwiseAnd(int left, int right)
+    {
+        var mask = 1 << 30;
+        int ans = 0;
+        while(mask > 0 && (left & mask) == (right & mask))
+        {
+            ans |= left & mask;
+            mask >>= 1;
+        }
+        return ans; 
+    }
+}
---
+++ code_hive/MyLinkedList/MyLinkedList/Program.cs
@@ -0,0 +1,92 @@
+﻿/// <summary>
+/// 20220923
+/// https://leetcode.cn/problems/design-linked-list/
+/// </summary>
+public class MyLinkedList
+{
+    private ListNode Root { get; set; }
+    private int Size { get; set; }
+    public MyLinkedList()
+    {
+        Root = new ListNode(-1);
+        Size = 0;
+    }
+
+    public int Get(int index)
+    {
+        if (index < 0 || index >= Size)
+        {
+            return -1;
+        }
+        ListNode cur = Root;
+        for (int i = 0; i <= index; i++)
+        {
+            cur = cur.next!;
+        }
+        return cur.val;
+    }
+
+    public void AddAtHead(int val)
+    {
+        AddAtIndex(0, val);
+    }
+
+    public void AddAtTail(int val)
+    {
+        AddAtIndex(Size, val);
+    }
+
+    public void AddAtIndex(int index, int val)
+    {
+        if (index > Size)
+        {
+            return;
+        }
+        index = Math.Max(0, index);
+        Size++;
+        var pred = Root;
+        for (int i = 0; i < index; i++)
+        {
+            pred = pred.next!;
+        }
+        var toAdd = new ListNode(val)
+        {
+            next = pred.next
+        };
+        pred.next = toAdd;
+    }
+
+    public void DeleteAtIndex(int index)
+    {
+        if (index < 0 || index >= Size)
+        {
+            return;
+        }
+        Size--;
+        var pred = Root;
+        for (int i = 0; i < index; i++)
+        {
+            pred = pred.next!;
+        }
+        pred.next = pred.next!.next;
+    }
+}
+
+
+class ListNode
+{
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val) => this.val = val;
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.Get(index);
+ * obj.AddAtHead(val);
+ * obj.AddAtTail(val);
+ * obj.AddAtIndex(index,val);
+ * obj.DeleteAtIndex(index);
+ */
