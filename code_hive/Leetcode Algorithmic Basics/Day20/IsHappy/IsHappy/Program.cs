5a98a001c3f8cc59c879f233c92349b2d489023b
Cary hu <cary.hu@grapecity.com>
2022-09-24 15:59:27 +0800

    20220924

---
+++ code_hive/Leetcode Algorithmic Basics/Day20/IsHappy/IsHappy/Program.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// 20220924
+/// https://leetcode.cn/problems/happy-number/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        var dict = new Dictionary<int, int>();
+        int m = n;
+        while (true)
+        {
+            int sum = 0;
+            while (m != 0)
+            {
+                sum += (m % 10) * (m % 10);
+                m /= 10;
+            }
+            if (sum == 1)
+            {
+                return true;
+            }
+            else if (dict.ContainsKey(sum))
+            {
+                return false;
+            }
+            else
+            {
+                dict.Add(sum, 0);
+            }
+
+            m = sum;
+        }
+    }
+}
---
+++ code_hive/Leetcode Algorithmic Basics/Day20/Shuffle/Shuffle/Program.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// 20220924
+/// https://leetcode.cn/problems/shuffle-an-array/?plan=algorithms&plan_progress=zd0dlym
+/// </summary>
+public class Solution
+{
+    private Random RandomP { get; set; } = new Random();
+
+    private int[] Nums { get; set; }
+    public Solution(int[] nums)
+    {
+        Nums = nums;
+    }
+
+    public int[] Reset()
+    {
+        return Nums;
+    }
+
+    public int[] Shuffle()
+    {
+        var res = new List<int>(Nums);
+        for (int i = 0; i < res.Count; i++)
+        {
+            int randomIndex = RandomP.Next() % res.Count;
+            if(randomIndex != i)
+            {
+                (res[i], res[randomIndex]) = (res[randomIndex], res[i]);
+            }
+
+        }
+        return res.ToArray();
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.Reset();
+ * int[] param_2 = obj.Shuffle();
+ */
