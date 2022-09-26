684cc7e9e592d06f45f6d439a961bc2b2e24e557
Cary Hu <cary.hu@grapecity.com>
2022-09-26 09:34:33 +0800

    20220926

---
+++ code_hive/MissingTwo/MissingTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// 20220926
+/// https://leetcode.cn/problems/missing-two-lcci/
+/// </summary>
+public class Solution
+{
+    public int[] MissingTwo(int[] nums)
+    {
+        int n = nums.Length + 2;
+        int sum = 0;
+        foreach (int i in nums) sum += i;
+        int ts = (1 + n) * n / 2 - sum;
+        int m = ts / 2;
+
+        sum = 0;
+        foreach (int i in nums)
+        {
+            if (i <= m) sum += i;
+        }
+        int res = (1 + m) * m / 2 - sum;
+        return new int[] { res, ts - res };
+    }
+}
