3ef8c892a0a04ba6094cfa9341eb423cc8ad4a09
Cary Hu <cary.hu@grapecity.com>
2022-09-27 08:33:32 +0800

    20220927

---
+++ code_hive/CheckPermutation/CheckPermutation/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20220927
+/// https://leetcode.cn/problems/check-permutation-lcci/
+/// </summary>
+public class Solution
+{
+    public bool CheckPermutation(string s1, string s2)
+    {
+        var sum1 = 0;
+        var sum2 = 0;
+        if (s1.Length != s2.Length)
+        {
+            return false;
+        }
+        return string.Join("", s1.OrderBy(x => x)) == string.Join("", s2.OrderBy(x => x));
+    }
+}
