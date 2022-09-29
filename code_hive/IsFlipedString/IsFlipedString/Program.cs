7b8e2bc339e2741e2285939ef8708ae2f2114b77
Cary Hu <cary.hu@grapecity.com>
2022-09-29 08:37:08 +0800

    20220929

---
+++ code_hive/IsFlipedString/IsFlipedString/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 20220929
+/// https://leetcode.cn/problems/string-rotation-lcci/
+/// </summary>
+public class Solution
+{
+    public bool IsFlipedString(string s1, string s2)
+    {
+        if (s1.Length != s2.Length)
+        {
+            return false;
+        }
+        return (s1 + s1).Contains(s2);
+    }
+}
