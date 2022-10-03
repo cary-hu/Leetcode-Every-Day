5351b4efef90fcf04fa45e74a6dc9a6d806449d6
Cary Hu <cary.hu@grapecity.com>
2022-10-03 09:36:37 +0800

    20221003

---
+++ code_hive/CheckOnesSegment/CheckOnesSegment/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 20221003
+/// https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+/// </summary>
+public class Solution
+{
+    public bool CheckOnesSegment(string s)
+    {
+        return !s.Contains("01");
+    }
+}
