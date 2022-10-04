b2beb1eb2e736a45bd0fa5490ad0090a4ec9e102
Cary Hu <cary.hu@grapecity.com>
2022-10-04 09:31:51 +0800

    20221004

---
+++ code_hive/MinAddToMakeValid/MinAddToMakeValid/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20221004
+/// https://leetcode.cn/problems/minimum-add-to-make-parentheses-valid/
+/// </summary>
+public class Solution
+{
+    public int MinAddToMakeValid(string s)
+    {
+        var res = 0;
+        var stack = new Stack<char>();
+        foreach (var charItem in s)
+        {
+            if (stack.Count <= 0)
+            {
+                stack.Push(charItem);
+                res++;
+                continue;
+            }
+            if (stack.Peek() == '(' && charItem == ')')
+            {
+                stack.Pop();
+                res--;
+                continue;
+            }
+            stack.Push(charItem);
+            res++;
+        }
+        return res;
+    }
+}
