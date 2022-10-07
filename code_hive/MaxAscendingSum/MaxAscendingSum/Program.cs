a2f935228e2d5cc45a6532429803ed4585ef9657
Cary <305997735@qq.com>
2022-10-07 11:41:04 +0800

    20221007

---
+++ code_hive/MaxAscendingSum/MaxAscendingSum/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 20221007
+/// https://leetcode.cn/problems/maximum-ascending-subarray-sum/
+/// </summary>
+public class Solution
+{
+    public int MaxAscendingSum(int[] nums)
+    {
+        var res = 0;
+
+        var currentSum = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                currentSum += nums[i];
+                res = Math.Max(res, currentSum);
+            }
+            else
+            {
+                currentSum = nums[i];
+            }
+        }
+
+        return res;
+    }
+}
