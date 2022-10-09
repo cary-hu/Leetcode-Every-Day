b06c23fb3b4535806c0bfb327fef020515beb4f8
Cary Hu <cary.hu@grapecity.com>
2022-10-09 09:19:08 +0800

    20221009

---
+++ code_hive/ScoreOfParentheses/ScoreOfParentheses/Program.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// 20221009
+/// https://leetcode.cn/problems/score-of-parentheses/
+/// </summary>
+public class Solution
+{
+    public int ScoreOfParentheses(string s)
+    {
+        return DFS(s, 1);
+    }
+    private int DFS(string s, int current)
+    {
+        if (s == "()")
+        {
+            return 1;
+        }
+        if (current == 2)
+        {
+            s = s[1..^1];
+        }
+        int res = 0;
+        int l = 0, r = 0, p = 0;
+        while (r < s.Length)
+        {
+            if (s[r] == '(') p++;
+            else p--;
+            if (p == 0)
+            {
+                res += DFS(s.Substring(l, r - l + 1), 2);
+                l = r + 1;
+            }
+            r++;
+        }
+        return current * res;
+    }
+}
