880d7d719bff62648a230964ed6336a038739d3c
Cary Hu <cary.hu@grapecity.com>
2022-10-13 08:52:22 +0800

    20221013

--- code_hive/MaxChunksToSorted/MaxChunksToSorted/Program.cs
+++ code_hive/MaxChunksToSorted/MaxChunksToSorted/Program.cs
@@ -1,9 +1,11 @@
-﻿/// <summary>
-/// 20220813
-/// https://leetcode.cn/problems/max-chunks-to-make-sorted-ii/
-/// </summary>
-public class Solution
+﻿public class Solution
 {
+    /// <summary>
+    /// 20221013
+    /// https://leetcode.cn/problems/max-chunks-to-make-sorted/
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
     public int MaxChunksToSorted(int[] arr)
     {
         var stack = new Stack<int>();
@@ -14,7 +16,31 @@ public class Solution
             {
                 element = Math.Max(arr[i], stack.Peek());
             }
-            while(stack.Count > 0 && arr[i] < stack.Peek())
+            while (stack.Count > 0 && arr[i] < stack.Peek())
+            {
+                stack.Pop();
+            }
+            stack.Push(element);
+        }
+        return stack.Count;
+    }
+    /// <summary>
+    /// 20220813
+    /// https://leetcode.cn/problems/max-chunks-to-make-sorted-ii/
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public int MaxChunksToSorted2(int[] arr)
+    {
+        var stack = new Stack<int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int element = arr[i];
+            if (stack.Count > 0)
+            {
+                element = Math.Max(arr[i], stack.Peek());
+            }
+            while (stack.Count > 0 && arr[i] < stack.Peek())
             {
                 stack.Pop();
             }
