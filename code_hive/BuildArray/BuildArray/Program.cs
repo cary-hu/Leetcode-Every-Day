5d0004ace8e33e1f901f5bfa38ff673fd1b7c4d9
Cary <305997735@qq.com>
2022-10-15 11:06:40 +0800

    20221015

---
+++ code_hive/BuildArray/BuildArray/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20221015
+/// https://leetcode.cn/problems/build-an-array-with-stack-operations/
+/// </summary>
+public class Solution
+{
+    public IList<string> BuildArray(int[] target, int n)
+    {
+        var res = new List<string>();
+        int i = 0;
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == target.Length)
+            {
+                break;
+            }
+            if (target[i] == j)
+            {
+                res.Add("Push");
+                i++;
+            }
+            else
+            {
+                res.Add("Push");
+                res.Add("Pop");
+            }
+        }
+        return res;
+    }
+}
