051f38e0a7ae89c41da72681a92f5a593c1002d6
Cary Hu <cary.hu@grapecity.com>
2022-10-17 08:52:32 +0800

    20221017

---
+++ code_hive/DistinctSubseqII/DistinctSubseqII/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20221014
+/// https://leetcode.cn/problems/distinct-subsequences-ii/
+/// </summary>
+public class Solution
+{
+    int mod = (int)1e9 + 7;
+    public int DistinctSubseqII(string s)
+    {
+        var count = new long[26];
+        var ans = 0l;
+        foreach (var charItem in s)
+        {
+            int a = charItem - 'a';
+            ans += mod - count[a];
+            count[a] = (count[a] + ans + 1) % mod;
+            ans = (ans + count[a]) % mod;
+        }
+        return (int)ans;
+    }
+}
