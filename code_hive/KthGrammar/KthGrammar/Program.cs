8aa66fe93af9341798a76b9d0b79ede8f8d913ef
Cary Hu <cary.hu@grapecity.com>
2022-10-20 08:57:32 +0800

    20221020

---
+++ code_hive/KthGrammar/KthGrammar/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 20221020
+/// https://leetcode.cn/problems/k-th-symbol-in-grammar/
+/// </summary>
+public class Solution
+{
+    public int KthGrammar(int n, int k)
+    {
+        int res = 0;
+        k--;
+        while (k != 0)
+        {
+            res ^= k & 1;
+            k >>= 1;
+        }
+        return res;
+    }
+
+}
