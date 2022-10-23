7299e9a29f8290c81fa312e66d89ad0b416dc5af
Cary <305997735@qq.com>
2022-10-23 10:54:31 +0800

    20221023

---
+++ code_hive/MergeAlternately/MergeAlternately/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+/// <summary>
+/// 20221023
+/// https://leetcode.cn/problems/merge-strings-alternately/
+/// </summary>
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        var sb = new StringBuilder();
+        var n = Math.Max(word1.Length, word2.Length);
+        for (int i = 0; i < n; i++)
+        {
+            if(i < word1.Length)
+            {
+                sb.Append(word1[i]);
+            }
+            if(i < word2.Length)
+            {
+                sb.Append(word2[i]);
+            }
+        }
+        return sb.ToString();
+    }
+}
