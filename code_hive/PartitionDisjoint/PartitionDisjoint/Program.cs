372155107e84c3dcff95fb5f900ce92b69a26db2
Cary Hu <cary.hu@grapecity.com>
2022-10-24 08:44:00 +0800

    20221024

---
+++ code_hive/PartitionDisjoint/PartitionDisjoint/Program.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// 20221024
+/// https://leetcode.cn/problems/partition-array-into-disjoint-intervals/
+/// </summary>
+public class Solution
+{
+    public int PartitionDisjoint(int[] nums)
+    {
+        var leftMax = nums[0];
+        var max = nums[0];
+        var index = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            max = Math.Max(max, nums[i]);
+            if (nums[i] < leftMax)
+            {
+                leftMax = max;
+                index = i;
+            }
+        }
+        return index + 1;
+    }
+}
