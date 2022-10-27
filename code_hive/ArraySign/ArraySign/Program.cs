5b94ba190c9a2e3fda935c7caf018a1f2a48ba86
Cary Hu <cary.hu@grapecity.com>
2022-10-27 08:38:05 +0800

    20221027

---
+++ code_hive/ArraySign/ArraySign/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20221027
+/// https://leetcode.cn/problems/sign-of-the-product-of-an-array/
+/// </summary>
+public class Solution
+{
+    public int ArraySign(int[] nums)
+    {
+        var res = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                continue;
+            } else if (nums[i] < 0)
+            {
+                res *= -1;
+            } else
+            {
+                res = 0;
+                break;
+            }
+        }
+        return res;
+    }
+}
