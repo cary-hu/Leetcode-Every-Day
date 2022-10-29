29eef5fb4b1d7512d96ffc9e84fca93493ec5fb9
Cary <305997735@qq.com>
2022-10-29 09:48:44 +0800

    20221029

---
+++ code_hive/CountMatches/CountMatches/Program.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// 20221029
+/// https://leetcode.cn/problems/count-items-matching-a-rule/
+/// </summary>
+public class Solution
+{
+    public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+    {
+        var res = 0;
+        var matchIndex = -1;
+        switch (ruleKey)
+        {
+            case "type":
+                matchIndex = 0; break;
+            case "color":
+                matchIndex = 1; break;
+            case "name":
+                matchIndex = 2; break;
+            default:
+                break;
+        }
+        if (matchIndex == -1)
+        {
+            return res;
+        }
+        foreach (var item in items)
+        {
+            if (item[matchIndex] == ruleValue)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
