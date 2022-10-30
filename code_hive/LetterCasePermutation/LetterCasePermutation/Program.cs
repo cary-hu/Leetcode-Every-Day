f28b7d8f054af66d7fed6c7e9f762d2ad5a9b835
Cary <305997735@qq.com>
2022-10-30 19:08:57 +0800

    20221030

---
+++ code_hive/LetterCasePermutation/LetterCasePermutation/Program.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// 20221030
+/// https://leetcode.cn/problems/letter-case-permutation/
+/// </summary>
+public class Solution
+{
+    private List<string> res = new List<string>();
+    public IList<string> LetterCasePermutation(string s)
+    {
+        Backtrack("", s.ToLower(), 0);
+        return res;
+    }
+    public void Backtrack(string currentString, string targetString, int index)
+    {
+        if (currentString.Length == targetString.Length)
+        {
+            res.Add(currentString);
+            return;
+        }
+        var currentChar = targetString[index++];
+        if (char.IsDigit(currentChar))
+        {
+            currentString += currentChar;
+            Backtrack(currentString, targetString, index);
+        } else
+        {
+            Backtrack(currentString + currentChar.ToString().ToLower(), targetString, index);
+            Backtrack(currentString + currentChar.ToString().ToUpper(), targetString, index);
+        }
+    }
+}
