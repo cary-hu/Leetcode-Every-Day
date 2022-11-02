fee79c367501170ef580d9f6dfd7082d0bae16d5
Cary Hu <cary.hu@grapecity.com>
2022-11-02 08:36:18 +0800

    20221102

---
+++ code_hive/BestCoordinate/BestCoordinate/Program.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// 20221102
+/// https://leetcode.cn/problems/coordinate-with-maximum-network-quality/
+/// </summary>
+public class Solution
+{
+    public int[] BestCoordinate(int[][] towers, int radius)
+    {
+        int maxq = 0, resx = 0, resy = 0;
+        for (int i = 0; i <= 50; i++)
+        {
+            for (int j = 0; j <= 50; j++)
+            {
+                int quality = 0;
+                foreach (var tower in towers)
+                {
+                    int x = tower[0], y = tower[1], q = tower[2];
+                    int d2 = (x - i) * (x - i) + (y - j) * (y - j);
+                    if (d2 <= radius * radius)
+                    {
+                        quality += (int)(q / (1 + Math.Sqrt(d2)));
+                    }
+                }
+                if (maxq < quality)
+                {
+                    maxq = quality;
+                    resx = i;
+                    resy = j;
+                }
+            }
+        }
+        return new int[] { resx, resy };
+    }
+}
