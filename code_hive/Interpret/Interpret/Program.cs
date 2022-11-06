1ee4e90761b6b2a01720a81eeb47a2bda3322b7b
Cary <305997735@qq.com>
2022-11-06 12:16:21 +0800

    20221106

---
+++ code_hive/Interpret/Interpret/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 20221106
+/// https://leetcode.cn/problems/goal-parser-interpretation/
+/// </summary>
+public class Solution
+{
+    public string Interpret(string command)
+    {
+        return command.Replace("()", "o").Replace("(al)", "al");
+    }
+}
