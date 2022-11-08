56f6aa54d0e87c5ef02e8c2ddb758318034bb733
Cary Hu <cary.hu@grapecity.com>
2022-11-08 08:31:54 +0800

    20221108

---
+++ code_hive/CountConsistentStrings/CountConsistentStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// 20221108
+/// https://leetcode.cn/problems/count-the-number-of-consistent-strings/
+/// </summary>
+public class Solution
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+        int ans = words.Length;
+        for (int i = 0; i < words.Length; i++)
+        {
+            foreach (char c in words[i].ToArray())
+            {
+                if (!allowed.Contains(c))
+                {
+                    ans--;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
