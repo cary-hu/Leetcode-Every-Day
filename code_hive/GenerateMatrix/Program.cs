fc5cb0d4b4a3b08e302d0444adb3bf22520e8f7e
Cary Hu <cary.hu@grapecity.com>
2022-11-11 13:53:45 +0800

    20221111

---
+++ code_hive/GenerateMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20221111
+/// https://leetcode.cn/problems/spiral-matrix-ii/
+/// </summary>
+public class Solution
+{
+    public int[][] GenerateMatrix(int n)
+    {
+        var res = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = new int[n];
+        }
+        var x = 0;
+        var y = 0;
+        res[x][y] = 1;
+        for (int i = 2; i <= n * n;)
+        {
+            while (y < n - 1 && res[x][y + 1] == 0) res[x][++y] = i++;
+            while (x < n - 1 && res[x + 1][y] == 0) res[++x][y] = i++;
+            while (y > 0 && res[x][y - 1] == 0) res[x][--y] = i++;
+            while (x > 0 && res[x - 1][y] == 0) res[--x][y] = i++;
+        }
+        return res;
+    }
+}
---
+++ code_hive/HalvesAreAlike/Program.cs
@@ -0,0 +1,69 @@
+﻿/// <summary>
+/// 20221111
+/// https://leetcode.cn/problems/determine-if-string-halves-are-alike/
+/// </summary>
+public class Solution
+{
+    public bool HalvesAreAlike(string s)
+    {
+        var halfLength = s.Length / 2;
+        var leftVowel = 0;
+        var rightVowel = 0;
+        for (int i = 0; i < halfLength; i++)
+        {
+            if (CheckIsVowel(s[i]))
+            {
+                leftVowel++;
+            }
+            if (CheckIsVowel(s[i + halfLength]))
+            {
+                rightVowel++;
+            }
+        }
+        return leftVowel == rightVowel;
+    }
+    private bool CheckIsVowel(char c)
+    {
+        if (c == 'a')
+        {
+            return true;
+        }
+        if (c == 'e')
+        {
+            return true;
+        }
+        if (c == 'i')
+        {
+            return true;
+        }
+        if (c == 'o')
+        {
+            return true;
+        }
+        if (c == 'u')
+        {
+            return true;
+        }
+        if (c == 'A')
+        {
+            return true;
+        }
+        if (c == 'E')
+        {
+            return true;
+        }
+        if (c == 'I')
+        {
+            return true;
+        }
+        if (c == 'O')
+        {
+            return true;
+        }
+        if (c == 'U')
+        {
+            return true;
+        }
+        return false;
+    }
+}
