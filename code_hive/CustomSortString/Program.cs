676ea98832278e77e8f0794f00db1a230192c3e4
Cary <305997735@qq.com>
2022-11-13 10:52:52 +0800

    20221113

---
+++ code_hive/CustomSortString/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 20221113
+/// https://leetcode.cn/problems/custom-sort-string
+/// </summary>
+public class Solution
+{
+    public string CustomSortString(string order, string s)
+    {
+        var OrderDict = new int[26];
+        Array.Fill(OrderDict, 0);
+        for (int i = 0; i < order.Length; i++)
+        {
+            OrderDict[order[i] - 'a'] = i + 1;
+        }
+        var res = s.ToArray();
+        Array.Sort(res, (a, b) =>OrderDict[a - 'a'] - OrderDict[b - 'a']);
+        return new string(res);
+    }
+}
