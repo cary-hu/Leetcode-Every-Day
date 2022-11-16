4a663c92719d66b705b650a09fdaec7a8dd5989a
Cary Hu <cary.hu@grapecity.com>
2022-11-16 08:59:42 +0800

    20221116

---
+++ code_hive/IsIdealPermutation/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20221116
+/// https://leetcode.cn/problems/global-and-local-inversions/
+/// </summary>
+public class Solution
+{
+    public bool IsIdealPermutation(int[] nums)
+    {
+        int max = nums[0];
+        for (int i = 2; i < nums.Length; i++)
+        {
+            if (nums[i] < max) { return false; }
+            max = Math.Max(max, nums[i - 1]);
+        }
+        return true;
+    }
+}
