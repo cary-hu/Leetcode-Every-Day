fe5c4828534c654023368860c7de6ac3cafc6e89
Cary <305997735@qq.com>
2022-11-17 20:54:30 +0800

    20221117

---
+++ code_hive/NumMatchingSubseq/Program.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// 20221117
+/// https://leetcode.cn/problems/number-of-matching-subsequences
+/// </summary>
+public class Solution
+{
+    public int NumMatchingSubseq(string s, string[] words)
+    {
+        var res = 0;
+        foreach (var word in words)
+        {
+            for (int i = 0, j = 0; i < word.Length;)
+            {
+                if (j >= s.Length)
+                {
+                    break;
+                }
+                if (word[i] == s[j])
+                {
+                    i++;
+                    j++;
+                    if (i == word.Length)
+                    {
+                        res++;
+                    }
+                    continue;
+                }
+                j++;
+            }
+        }
+        return res;
+    }
+}
