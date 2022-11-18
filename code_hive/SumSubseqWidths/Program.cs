935f4846d122dcf8f7efb5c7e97a229ee2f62e5b
Cary Hu <cary.hu@grapecity.com>
2022-11-18 13:45:04 +0800

    20221118

---
+++ code_hive/SumSubseqWidths/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20221118
+/// https://leetcode.cn/problems/sum-of-subsequence-widths/
+/// </summary>
+public class Solution
+{
+    public int SumSubseqWidths(int[] nums)
+    {
+        const int MOD = 1000000007;
+        Array.Sort(nums);
+        long res = 0;
+        long x = nums[0], y = 2;
+        for (int j = 1; j < nums.Length; j++)
+        {
+            res = (res + nums[j] * (y - 1) - x) % MOD;
+            x = (x * 2 + nums[j]) % MOD;
+            y = y * 2 % MOD;
+        }
+        return (int)((res + MOD) % MOD);
+    }
+}
