03f66a1713e015150fcddd9c0288d0e0fcc64a6a
Cary <305997735@qq.com>
2022-11-19 10:27:01 +0800

    20221119

---
+++ code_hive/LargestAltitude/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 20221119
+/// https://leetcode.cn/problems/find-the-highest-altitude/
+/// </summary>
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        var maxGain = 0;
+        var sum = 0;
+        foreach (var gainItem in gain)
+        {
+            sum += gainItem;
+            maxGain = Math.Max(sum, maxGain);
+        }
+        return maxGain;
+    }
+}
