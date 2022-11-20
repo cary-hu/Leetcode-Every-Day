7cff8ebb666b42e45a65b3f085aef580dfb7271c
Cary Hu <cary.hu@grapecity.com>
2022-11-20 12:56:32 +0800

    20221120

--- fakedatehook.sh
+++ fakedatehook.sh
@@ -2,8 +2,8 @@
 
 _() {
   YEAR="2022"
-  MONTH="10"
-  DAY="14"
+  MONTH="11"
+  DAY="20"
   git add .
   GIT_AUTHOR_DATE="${YEAR}-${MONTH}-${DAY}T12:56:32" \
     GIT_COMMITTER_DATE="${YEAR}-${MONTH}-${DAY}T12:56:32" \
