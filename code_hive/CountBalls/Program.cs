970e795d14bc9654bd33fb85349a3d50b0722209
Cary Hu <cary.hu@grapecity.com>
2022-11-23 09:34:15 +0800

    20221123

---
+++ code_hive/CountBalls/Program.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// 20221123
+/// https://leetcode.cn/problems/maximum-number-of-balls-in-a-box/
+/// </summary>
+public class Solution
+{
+    public int CountBalls(int lowLimit, int highLimit)
+    {
+        var counter = new int[46];
+        Array.Fill(counter, 0);
+        for (int i = lowLimit; i <= highLimit; i++)
+        {
+            counter[BallSum(i)]++;
+        }
+        return counter.Max();
+    }
+    private int BallSum(int ball)
+    {
+        var res = 0;
+        while (ball != 0)
+        {
+            res += ball % 10;
+            ball /= 10;
+        }
+        return res;
+    }
+}
