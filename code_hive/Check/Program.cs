7b9f5bc1a57ad39ce6af3ce41dea4d66af45bac0
Cary <305997735@qq.com>
2022-11-27 10:25:45 +0800

    20221127

---
+++ code_hive/Check/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20221127
+/// https://leetcode.cn/problems/check-if-array-is-sorted-and-rotated/
+/// </summary>
+public class Solution
+{
+    public bool Check(int[] nums)
+    {
+        int i = 0, n = nums.Length;
+        var flag = true;
+        while (++i < n)
+        {
+            if (nums[i] < nums[i - 1])
+            {
+                if (!flag) return false;
+                flag = false;
+            }
+        }
+        return nums[n - 1] <= nums[0] || flag;
+    }
+}
