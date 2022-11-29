ce16e9edc86f9da90d4f15e85cf580c114f384bd
Cary Hu <cary.hu@grapecity.com>
2022-11-29 08:30:08 +0800

    20221129

---
+++ code_hive/MinOperations2/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20221129
+/// https://leetcode.cn/problems/minimum-changes-to-make-alternating-binary-string/
+/// </summary>
+public class Solution
+{
+    public int MinOperations(string s)
+    {
+        var x = 0;
+        int cnt0 = 0, cnt1 = 0;
+        foreach (char c in s)
+        {
+            if (c-'0' != x)
+            {
+                cnt0++;
+            }
+            else
+            {
+                cnt1++;
+            }
+            x ^= 1;
+        }
+        return Math.Min(cnt0, cnt1);
+    }
+}
