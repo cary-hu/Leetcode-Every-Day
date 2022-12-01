e23103f56a7853aee648458a9127432539c11aee
Cary <305997735@qq.com>
2022-12-01 19:41:49 +0800

    20221201

---
+++ code_hive/NearestValidPoint/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+/// <summary>
+/// 20221201
+/// https://leetcode.cn/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+/// </summary>
+public class Solution
+{
+    public int NearestValidPoint(int x, int y, int[][] points)
+    {
+        var res = -1;
+        var currentLength = int.MaxValue;
+        for (int i = 0; i < points.Length; i++)
+        {
+            var point = points[i];
+            if (!IsValidPoint(x, y, point))
+            {
+                continue;
+            }
+            var manhadon = GetManhadon(x, y, point);
+            if (manhadon < currentLength)
+            {
+                currentLength = manhadon;
+                res = i;
+            }
+        }
+        return res;
+    }
+    private int GetManhadon(int x, int y, int[] points)
+    {
+        return Math.Abs(points[0] - x) + Math.Abs(points[1] - y);
+    }
+    private bool IsValidPoint(int x, int y, int[] point)
+    {
+        if (point[0] == x)
+        {
+            return true;
+        }
+        if (point[1] == y)
+        {
+            return true;
+        }
+        return false;
+    }
+}
