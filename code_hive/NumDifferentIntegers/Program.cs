da489156664f37fee1b14d210eefa6bd1f044df3
Cary <305997735@qq.com>
2022-12-06 20:34:52 +0800

    20221206

---
+++ code_hive/NumDifferentIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// 20221206
+/// https://leetcode.cn/problems/number-of-different-integers-in-a-string/
+/// </summary>
+public class Solution
+{
+    public int NumDifferentIntegers(string word)
+    {
+        ISet<string> set = new HashSet<string>();
+        int n = word.Length, p1 = 0, p2;
+        while (true)
+        {
+            while (p1 < n && !char.IsDigit(word[p1]))
+            {
+                p1++;
+            }
+            if (p1 == n)
+            {
+                break;
+            }
+            p2 = p1;
+            while (p2 < n && char.IsDigit(word[p2]))
+            {
+                p2++;
+            }
+            while (p2 - p1 > 1 && word[p1] == '0')
+            {
+                p1++;
+            }
+            set.Add(word[p1..p2]);
+            p1 = p2;
+        }
+        return set.Count;
+    }
+}
