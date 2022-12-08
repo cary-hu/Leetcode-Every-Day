9e9c1defbd3253796f0fb61359b270bdae6741f2
Cary <305997735@qq.com>
2022-12-08 20:08:03 +0800

    20221208

---
+++ code_hive/SquareIsWhite/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 20221208
+/// https://leetcode.cn/problems/determine-color-of-a-chessboard-square/
+/// </summary>
+public class Solution
+{
+    public bool SquareIsWhite(string coordinates)
+    {
+        var num1 = coordinates[0] - 'a';
+        var num2 = coordinates[1] - '0';
+        return (num1 + num2) % 2 == 0;
+    }
+}
