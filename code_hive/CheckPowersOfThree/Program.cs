660badca1c4f0b37df0503b389ed6439f8f17400
Cary Hu <cary.hu@grapecity.com>
2022-12-09 13:35:59 +0800

    20221209

---
+++ code_hive/CheckPowersOfThree/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20221209
+/// https://leetcode.cn/problems/check-if-number-is-a-sum-of-powers-of-three/
+/// </summary>
+public class Solution
+{
+    public bool CheckPowersOfThree(int n)
+    {
+        while (n != 0)
+        {
+            if (n % 3 > 1)
+                return false;
+            n /= 3;
+        }
+        return true;
+    }
+}
