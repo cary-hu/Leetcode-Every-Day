1a91f270f92a7718f4c775d92cd18ab9d42c8bb6
Cary Hu <cary.hu@grapecity.com>
2022-12-11 11:48:34 +0800

    20221211

---
+++ code_hive/MinOperations5/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20221211
+/// https://leetcode.cn/problems/minimum-operations-to-make-the-array-increasing/
+/// </summary>
+public class Solution
+{
+    public int MinOperations(int[] nums)
+    {
+        var res = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            res += Math.Max(0, nums[i - 1] + 1 - nums[i]);
+            nums[i] = Math.Max(nums[i], nums[i - 1] + 1);
+        }
+        return res;
+    }
+}
