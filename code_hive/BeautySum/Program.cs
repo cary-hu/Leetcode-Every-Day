b1fa5489d593c99dd4c3f4f9ee47e073c92af186
Cary Hu <cary.hu@grapecity.com>
2022-12-12 17:14:06 +0800

    20221212

---
+++ code_hive/BeautySum/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20221212
+/// https://leetcode.cn/problems/sum-of-beauty-of-all-substrings/
+/// </summary>
+public class Solution
+{
+    public int BeautySum(string s)
+    {
+        int n = s.Length;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int[] map = new int[26];
+            for (int j = i; j < n; j++)
+            {
+                map[s[j] - 'a']++;
+                int max = 0, min = s.Length + 1;
+                for (int p = 0; p < map.Length; p++)
+                {
+                    if (map[p] == 0) continue;
+                    max = Math.Max(max, map[p]);
+                    min = Math.Min(min, map[p]);
+                }
+                sum += (max - min);
+            }
+
+        }
+        return sum;
+    }
+}
