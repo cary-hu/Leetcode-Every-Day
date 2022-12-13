812b9f8cf8c1774b59cdc62ba7cd2c087bcad189
Cary <305997735@qq.com>
2022-12-13 20:16:19 +0800

    20221213

---
+++ code_hive/CheckIfPangram/Program.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 20221213
+/// https://leetcode.cn/problems/check-if-the-sentence-is-pangram/
+/// </summary>
+public class Solution
+{
+    public bool CheckIfPangram(string sentence)
+    {
+        var dict = new HashSet<char>();
+        foreach (var charItem in sentence)
+        {
+            dict.Add(charItem);
+        }
+        return dict.Count == 26;
+    }
+}
