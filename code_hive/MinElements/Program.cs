ff18054e81592056f363fd94ec0aca151e97100a
Cary Hu <cary.hu@grapecity.com>
2022-12-16 14:00:38 +0800

    20221216

---
+++ code_hive/MinElements/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20221216
+/// https://leetcode.cn/problems/minimum-elements-to-add-to-form-a-given-sum/
+/// </summary>
+public class Solution
+{
+    public int MinElements(int[] nums, int limit, int goal)
+    {
+        long currentSum = 0;
+        foreach (var item in nums)
+        {
+            currentSum += item;
+        }
+        var diff = Math.Abs(currentSum - goal);
+        return (int)((diff + limit - 1) / limit);
+    }
+}
