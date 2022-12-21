8d651acb4ab962660510b38c6daacb9771e3ff85
Cary Hu <cary.hu@grapecity.com>
2022-12-21 17:16:14 +0800

    20221221

---
+++ code_hive/MaximumScore/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 20221221
+/// https://leetcode.cn/problems/maximum-score-from-removing-stones/
+/// </summary>
+public class Solution
+{
+    public int MaximumScore(int a, int b, int c)
+    {
+        int sum = a + b + c;
+        int maxVal = Math.Max(Math.Max(a, b), c);
+        return Math.Min(sum - maxVal, sum / 2);
+    }
+}
