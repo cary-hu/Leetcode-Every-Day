92ad5dc7bddc08eb24bcfb014b6000a027e70e0c
Cary Hu <cary.hu@grapecity.com>
2022-12-23 13:35:49 +0800

    20221223

---
+++ code_hive/FinalValueAfterOperations/Program.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// 20221223
+/// https://leetcode.cn/problems/final-value-of-variable-after-performing-operations/
+/// </summary>
+public class Solution
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        var res = 0;
+        foreach (var operation in operations)
+        {
+            if (operation[1] == '+')
+            {
+                res++;
+            } else
+            {
+                res--;
+            }
+        }
+        return res;
+    }
+}
