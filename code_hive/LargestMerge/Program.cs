79688e0e10295d3f1d19828557364050f45c0294
Cary <305997735@qq.com>
2022-12-24 10:12:03 +0800

    20221224

---
+++ code_hive/LargestMerge/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20221224
+/// https://leetcode.cn/problems/largest-merge-of-two-strings/
+/// </summary>
+public class Solution
+{
+    public string LargestMerge(string word1, string word2)
+    {
+        var res = "";
+        var i = 0;
+        var j = 0;
+        while (i < word1.Length || j < word2.Length)
+        {
+            if (i < word1.Length && word1[i..].CompareTo(word2[j..]) > 0)
+            {
+                res += word1[i++];
+            }
+            else
+            {
+                res += word2[j++];
+            }
+        }
+
+        return res;
+    }
+}
