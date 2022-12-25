870056c67fae43f7a5924efa7508cd5ed4437787
Cary <305997735@qq.com>
2022-12-25 11:41:07 +0800

    20221225

---
+++ code_hive/MinimumBoxes/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20221225
+/// https://leetcode.cn/problems/building-boxes/
+/// </summary>
+public class Solution
+{
+    public int MinimumBoxes(int n)
+    {
+        int cur = 1, i = 1, j = 1;
+        while (n > cur)
+        {
+            n -= cur;
+            i++;
+            cur += i;
+        }
+        cur = 1;
+        while (n > cur)
+        {
+            n -= cur;
+            j++;
+            cur++;
+        }
+        return (i - 1) * i / 2 + j;
+    }
+}
