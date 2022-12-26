bfcb419cf53f576356cfda6709467e9321f89e32
Cary Hu <cary.hu@grapecity.com>
2022-12-26 16:00:41 +0800

    20221226

---
+++ code_hive/CountHomogenous/Program.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// 20221226
+/// https://leetcode.cn/problems/count-number-of-homogenous-substrings/
+/// </summary>
+public class Solution
+{
+    public int CountHomogenous(string s)
+    {
+        const int MOD = 1000000007;
+        long res = 0;
+        char prev = s[0];
+        int cnt = 0;
+        foreach (char c in s)
+        {
+            if (c == prev)
+            {
+                cnt++;
+            }
+            else
+            {
+                res += (long)(cnt + 1) * cnt / 2;
+                cnt = 1;
+                prev = c;
+            }
+        }
+        res += (long)(cnt + 1) * cnt / 2;
+        return (int)(res % MOD);
+    }
+}
