cba9e341580211953e902d4a702d3ab81ca3ac39
Cary Hu <cary.hu@grapecity.com>
2022-12-27 12:04:55 +0800

    20221227

---
+++ code_hive/MinimumMoves/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20221227
+/// https://leetcode.cn/problems/minimum-moves-to-convert-string/
+/// </summary>
+public class Solution
+{
+    public int MinimumMoves(string s)
+    {
+        var n = s.Length;
+        var i = 0;
+        var res = 0;
+        while (i < n)
+        {
+            if (s[i] == 'X')
+            {
+                i += 3;
+                res++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return res;
+    }
+}
