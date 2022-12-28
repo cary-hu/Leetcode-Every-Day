8fa027fadd00e941988134c43b8955223e53cdfe
Cary <305997735@qq.com>
2022-12-28 15:36:31 +0800

    20221228

---
+++ code_hive/MinimumLength/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20221228
+/// https://leetcode.cn/problems/minimum-length-of-string-after-deleting-similar-ends/
+/// </summary>
+public class Solution
+{
+    public int MinimumLength(string s)
+    {
+        var l = 0;
+        var r = s.Length - 1;
+        while (l < r && s[l] == s[r])
+        {
+            while(l <= r && s[l] == s[r])
+            {
+                l++;
+            }
+
+            while (l < r && s[l - 1] == s[r])
+            {
+                r--;
+            }
+        }
+        return r - l + 1;
+    }
+}
