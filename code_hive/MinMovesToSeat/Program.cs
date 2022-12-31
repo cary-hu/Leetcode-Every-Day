3c5d00535cb1fb50477e7f7c7642e19709f29758
Cary <305997735@qq.com>
2022-12-31 14:40:43 +0800

    20221231

---
+++ code_hive/MinMovesToSeat/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 20221231
+/// https://leetcode.cn/problems/minimum-number-of-moves-to-seat-everyone/
+/// </summary>
+public class Solution
+{
+    public int MinMovesToSeat(int[] seats, int[] students)
+    {
+        Array.Sort(seats);
+        Array.Sort(students);
+        int ans = 0;
+        for (int i = 0; i < seats.Length; i++)
+        {
+            ans += Math.Abs(students[i] - seats[i]);
+        }
+        return ans;
+    }
+}
