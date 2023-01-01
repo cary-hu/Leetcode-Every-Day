4fb7d4f78d8c35d78babafbab2a5a4ca36dba01b
Cary <305997735@qq.com>
2023-01-01 14:05:24 +0800

    20230101

---
+++ code_hive/RepeatedCharacter/Program.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// 20230101
+/// https://leetcode.cn/problems/first-letter-to-appear-twice/
+/// </summary>
+public class Solution
+{
+    public char RepeatedCharacter(string s)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach (var charItem in s)
+        {
+            if (dict.ContainsKey(charItem))
+            {
+                return charItem;
+            }
+            dict.Add(charItem, 1);
+        }
+        return '';
+    }
+}
