ac312beb1c6d86d73b01eb1cd3967a8211e0ab4a
Cary Hu <cary.hu@grapecity.com>
2023-01-03 10:57:53 +0800

    20230103

---
+++ code_hive/AreNumbersAscending/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 20220103
+/// https://leetcode.cn/problems/check-if-numbers-are-ascending-in-a-sentence/
+/// </summary>
+public class Solution
+{
+    public bool AreNumbersAscending(string s)
+    {
+        var originNumber = s.Split(" ").Where(x => int.TryParse(x, out _)).Select(x => Convert.ToInt32(x)).ToList();
+        var sortedNumber = originNumber.OrderBy(x => x).Distinct();
+        return sortedNumber.SequenceEqual(originNumber);
+    }
+}
