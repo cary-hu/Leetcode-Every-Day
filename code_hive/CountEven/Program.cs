58db310fa0d8fda370e9b13de9b3cb5fd5542c83
Cary Hu <cary.hu@grapecity.com>
2023-01-06 13:36:02 +0800

    20230106

---
+++ code_hive/CountEven/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20220106
+/// https://leetcode.cn/problems/count-integers-with-even-digit-sum/
+/// </summary>
+public class Solution
+{
+    public int CountEven(int num)
+    {
+        var res = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            if (isEven(i)) res++;
+        }
+        return res;
+    }
+    public bool isEven(int num)
+    {
+        var sum = 0;
+        while (num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum % 2 == 0;
+    }
+}
