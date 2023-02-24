8c97c081e057ec18d08a4c3c87d3ffa3d4e34e89
cary <cary.hu@grapecity.com>
2023-02-24 13:35:38 +0800

    20230224

---
+++ code_hive/Largest1BorderedSquare/Program.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// 20230217
+/// https://leetcode.cn/problems/largest-1-bordered-square/
+/// </summary>
+public class Solution
+{
+    public int Largest1BorderedSquare(int[][] grid)
+    {
+        int maxItemCount = 1;
+
+
+        return maxItemCount;
+    }
+}
---
+++ code_hive/MinimumOperations/Program.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// 20230224
+/// https://leetcode.cn/problems/make-array-zero-by-subtracting-equal-amounts/
+/// </summary>
+public class Solution {
+    public int MinimumOperations(int[] nums) {
+        return nums.Distinct().Where(x => x > 0).Count();
+    }
+}
