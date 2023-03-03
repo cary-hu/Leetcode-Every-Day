be260ede19efd12c01b36ce766c5d761472f10ee
cary <cary.hu@grapecity.com>
2023-03-03 13:52:10 +0800

    20230303

---
+++ code_hive/GetFolderNames/Program.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// 20230303
+/// https://leetcode.cn/problems/making-file-names-unique/
+/// </summary>
+public class Solution {
+    public string[] GetFolderNames(string[] names) {
+        var _dict = new Dictionary<string, int>();
+        var n = names.Length;
+        var res = new string[n];
+        string resName;
+        for (int i = 0; i < n; i++) {
+            string name = names[i];
+            if (!_dict.ContainsKey(name)) {
+                resName = name;
+            }
+            else {
+                int k = _dict[name];
+                while (_dict.ContainsKey($"{name}({k})")) {
+                    k++;
+                }
+                resName = $"{name}({k})";
+                _dict[name] = k + 1;
+            }
+            res[i] = resName;
+            _dict.Add(resName, 1);
+        }
+        return res;
+    }
+}
