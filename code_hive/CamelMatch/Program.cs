9ff310f5f4520e8878fef70c89e476cf7e5fc212
CaryHu <cary.hu@developertools.com>
2023-04-14 14:01:34 +0800

    20230414

---
+++ code_hive/CamelMatch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+/// <summary>
+/// 20230414
+/// https://leetcode.cn/problems/camelcase-matching/
+/// </summary>
+public class Solution
+{
+    public IList<bool> CamelMatch(string[] queries, string pattern)
+    {
+        string patternRegex = @$"^[a-z]*?{string.Join("[a-z]*?", pattern.ToCharArray())}[a-z]*?$";
+        var res = new bool[queries.Length];
+        var index = 0;
+        foreach (var query in queries)
+        {
+            var isMatch = Regex.IsMatch(query, patternRegex);
+            res[index++] = isMatch;
+        }
+        return res;
+    }
+}
