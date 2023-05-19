20b3a4a0fda1c0e8c9f09bcaf0419811e5921d9d
CaryHu <cary.hu@developertools.com>
2023-05-19 13:50:04 +0800

    20230519

---
+++ code_hive/DinnerPlates/Program.cs
@@ -0,0 +1,83 @@
+﻿public class DinnerPlates
+{
+    private int capacity;
+    private Dictionary<int, LinkedList<int>> stacks;
+    private SortedSet<int> nonFullStacks;
+
+    public DinnerPlates(int capacity)
+    {
+        this.capacity = capacity;
+        this.stacks = new Dictionary<int, LinkedList<int>>();
+        this.nonFullStacks = new SortedSet<int>();
+    }
+
+    public void Push(int val)
+    {
+        int index;
+        if (nonFullStacks.Count == 0)
+        {
+            index = stacks.Count;
+            stacks[index] = new LinkedList<int>();
+        }
+        else
+        {
+            index = nonFullStacks.Min;
+        }
+        stacks[index].AddLast(val);
+        if (stacks[index].Count == capacity)
+        {
+            nonFullStacks.Remove(index);
+        }
+        else
+        {
+            nonFullStacks.Add(index);
+        }
+    }
+
+    public int Pop()
+    {
+        if (stacks.Count == 0)
+        {
+            return -1;
+        }
+        var lastStack = stacks[stacks.Count - 1];
+        while (lastStack.Count > 0 && lastStack.Last.Value == 0)
+        {
+            lastStack.RemoveLast();
+        }
+        if (lastStack.Count == 0)
+        {
+            stacks.Remove(stacks.Count - 1);
+            nonFullStacks.Remove(stacks.Count);
+            return Pop();
+        }
+        nonFullStacks.Add(stacks.Count - 1);
+        int result = lastStack.Last.Value;
+        lastStack.RemoveLast();
+        return result;
+    }
+
+    public int PopAtStack(int index)
+    {
+        if (!stacks.ContainsKey(index) || stacks[index].Count == 0)
+        {
+            return -1;
+        }
+        var stack = stacks[index];
+        while (stack.Count > 0 && stack.Last.Value == 0)
+        {
+            stack.RemoveLast();
+        }
+        if (index == stacks.Count - 1)
+        {
+            nonFullStacks.Add(stacks.Count - 1);
+        }
+        else if (!nonFullStacks.Contains(index))
+        {
+            nonFullStacks.Add(index);
+        }
+        int result = stack.Last.Value;
+        stack.RemoveLast();
+        return result;
+    }
+}
---
+++ code_hive/NumTilePossibilities/Program.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// 20230519
+/// https://leetcode.cn/problems/letter-tile-possibilities/
+/// </summary>
+public class Solution
+{
+    public int NumTilePossibilities(string tiles)
+    {
+        var count = new Dictionary<char, int>();
+        foreach (char t in tiles)
+        {
+            if (!count.ContainsKey(t))
+            {
+                count.Add(t, 0);
+            }
+            count[t]++;
+        }
+        return Dfs(tiles.Length, count, new HashSet<char>(count.Keys)) - 1;
+    }
+
+    private int Dfs(int i, Dictionary<char, int> count, HashSet<char> tile)
+    {
+        if (i == 0)
+        {
+            return 1;
+        }
+        int res = 1;
+        foreach (char t in tile)
+        {
+            if (count[t] > 0)
+            {
+                count[t]--;
+                res += Dfs(i - 1, count, tile);
+                count[t]++;
+            }
+        }
+        return res;
+    }
+}
