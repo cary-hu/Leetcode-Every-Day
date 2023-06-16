2b2fe2a9d93535202fb2bf30dc6647279e9e430d
CaryHu <cary.hu@developertools.com>
2023-06-16 13:54:11 +0800

    20230616

---
+++ code_hive/IsToeplitzMatrix/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20230616
+/// https://leetcode.cn/problems/toeplitz-matrix/
+/// </summary>
+public class Solution
+{
+    public bool IsToeplitzMatrix(int[][] matrix)
+    {
+        for (int i = matrix.Length - 1; i > 0; i--)
+        {
+            for (int j = 1; j < matrix[0].Length; j++)
+            {
+                if (matrix[i][j] != matrix[i - 1][j - 1])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
