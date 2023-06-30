a5b08f1fe5ba4613ca22cf9e981bb92013a2f7a7
CaryHu <cary.hu@developertools.com>
2023-06-30 13:35:16 +0800

    20230630

---
+++ code_hive/IsCircularSentence/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20230630
+/// https://leetcode.cn/problems/circular-sentence/
+/// </summary>
+public class Solution
+{
+    public bool IsCircularSentence(string sentence)
+    {
+        for (int i = 0; i < sentence.Length - 1; i++)
+        {
+            if (sentence[i] == ' ')
+            {
+                if (sentence[i - 1] != sentence[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        if (sentence[^1] == sentence[0])
+        {
+            return true;
+        }
+        return false;
+    }
+}
