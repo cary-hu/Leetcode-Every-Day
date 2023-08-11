e08ee3aaaf65df85d4ce44f8139be14024e48276
Carrrrrrrrry <Cary.hu@devlopertools.com>
2023-08-11 13:36:32 +0800

    20230811

---
+++ code_hive/DiagonalSum/Program.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// 20230811
+/// https://leetcode.cn/problems/matrix-diagonal-sum/
+/// </summary>
+public class Solution
+{
+    public int DiagonalSum(int[][] mat)
+    {
+        int n = mat.Length, sum = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (i == j || i + j == n - 1)
+                {
+                    sum += mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
