225d3d52b5046b66e47007361bffe51d18a2a9ea
Carrrrrrrrry <Cary.hu@devlopertools.com>
2023-09-15 13:35:53 +0800

    20230915

---
+++ code_hive/GiveGem/Program.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 20230915
+/// https://leetcode.cn/problems/WHnhjV
+/// </summary>
+public class Solution
+{
+    public int GiveGem(int[] gem, int[][] operations)
+    {
+        foreach (var operation in operations)
+        {
+            var gemCount = gem[operation[0]] >> 1;
+            gem[operation[0]] -= gemCount;
+            gem[operation[1]] += gemCount;
+        }
+        return gem.Max() - gem.Min();
+    }
+}
