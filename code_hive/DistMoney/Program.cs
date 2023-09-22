07e2ea62dea4b4340044f9f61125c04313c68085
Carrrrrrrrry <Cary.hu@devlopertools.com>
2023-09-22 13:35:54 +0800

    20230922

---
+++ code_hive/DistMoney/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 20230922
+/// https://leetcode.cn/problems/distribute-money-to-maximum-children
+/// </summary>
+public class Solution
+{
+    public int DistMoney(int money, int children)
+    {
+        if (money < children)
+        {
+            return -1;
+        }
+        int tmp = children * 8 - money;
+        if (tmp == 4) return children - 2;
+        if (tmp < 0) return children - 1;
+        return (money - children) / 7;
+    }
+}
