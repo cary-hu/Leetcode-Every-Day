d2fe1b685fedf1b6bab0432314f525ba25a8cc3d
Carrrrrrrrry <Cary.hu@devlopertools.com>
2023-10-20 13:43:07 +0800

    20231020

---
+++ code_hive/AvoidFlood/Program.cs
@@ -0,0 +1,70 @@
+﻿/// <summary>
+/// 20231013
+/// https://leetcode.cn/problems/avoid-flood-in-the-city/
+/// </summary>
+new Solution().AvoidFlood(new int[] { 1, 2, 3, 4 });
+public class LakeInfo
+{
+    public int LakeId { get; set; }
+    public bool IsFull { get; set; }
+}
+public class Solution
+{
+    public int[] AvoidFlood(int[] rains)
+    {
+        var res = new int[rains.Length];
+        Array.Fill(res, -1);
+
+        var notRainDays = new int[rains.Length];
+        Array.Fill(res, 0);
+
+        var lake = new List<LakeInfo>();
+
+        for (int i = 0; i < rains.Length; i++)
+        {
+            if (rains[i] == 0)
+            {
+                notRainDays[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < rains.Length; i++)
+        {
+            var lakeInfo = lake.Find(x => x.LakeId == rains[i]);
+            if (lakeInfo == null)
+            {
+                lakeInfo = new LakeInfo()
+                {
+                    LakeId = rains[i],
+                    IsFull = false
+                };
+                lake.Add(lakeInfo);
+            }
+            if (rains[i] >= 1)
+            {
+                if (lakeInfo.IsFull)
+                {
+                    for (int j = i + 1; j < notRainDays.Length; j++)
+                    {
+                        if (notRainDays[j] == 1)
+                        {
+                            lakeInfo.IsFull = false; notRainDays[j] = 0;
+                            res[j] = lakeInfo.LakeId;
+                            break;
+                        }
+                    }
+                    if (lakeInfo.IsFull)
+                    {
+                        return new int[0];
+                    }
+                    lakeInfo.IsFull = true;
+                }
+                else
+                {
+                    lakeInfo.IsFull = true;
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ code_hive/CategorizeBox/Program.cs
@@ -0,0 +1,53 @@
+﻿/// <summary>
+/// 20231020
+/// https://leetcode.cn/problems/categorize-box-according-to-criteria
+/// </summary>
+public class Solution
+{
+    public int MaxSize = 10000;
+    public int MaxVolume = 1000000000;
+    public string CategorizeBox(int length, int width, int height, int mass)
+    {
+        var isHeavyBox = IsHeavyBox(mass);
+        var isBulkyBox = IsBulkyBox(length, width, height);
+        if (isHeavyBox && isBulkyBox)
+        {
+            return "Both";
+        }
+        if (isHeavyBox && !isBulkyBox)
+        {
+            return "Heavy";
+        }
+        if (!isHeavyBox && isBulkyBox)
+        {
+            return "Bulky";
+
+        }
+        if (!isHeavyBox && !isBulkyBox)
+        {
+            return "Neither";
+        }
+        return "";
+    }
+    public bool IsHeavyBox(int mass)
+    {
+        if (mass >= 100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool IsBulkyBox(int length, int width, int height)
+    {
+        if (length >= MaxSize || width >= MaxSize || height >= MaxSize)
+        {
+            return true;
+        }
+        long volume = (long)length * (long)width * (long)height * 1l;
+        return volume >= MaxVolume;
+
+    }
+}
