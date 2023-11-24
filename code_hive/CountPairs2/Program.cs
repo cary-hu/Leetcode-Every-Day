e26b37252bfe5e9290453baab27188e051bf5f78
Cary <Cary.hu@devlopertools.com>
2023-11-24 13:54:09 +0800

    20231124

---
+++ code_hive/CountPairs2/Program.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// 20231124
+/// https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/
+/// </summary>
+public class Solution
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        var res = 0;
+        var n = nums.Count;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i < j && j < n && nums[i] + nums[j] < target)
+                {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
