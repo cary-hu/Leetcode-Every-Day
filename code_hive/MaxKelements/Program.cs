4de92b3ecf04b0fc9580130d01110b74aa9e6a33
Cary <Cary.hu@devlopertools.com>
2024-03-15 13:45:17 +0800

    20240315

---
+++ code_hive/MaxKelements/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20240315
+/// https://leetcode.cn/problems/maximal-score-after-applying-k-operations/
+/// </summary>
+public class Solution
+{
+    public long MaxKelements(int[] nums, int k)
+    {
+        var res = 0L;
+        var queue = new PriorityQueue<int, int>();
+        foreach (var item in nums)
+        {
+            queue.Enqueue(item, -item);
+        }
+        while (k > 0)
+        {
+            var item = queue.Dequeue();
+            res += item;
+            queue.Enqueue((int)Math.Ceiling(item / 3m), -(int)Math.Ceiling(item / 3m));
+            k--;
+        }
+
+        return res;
+    }
+}
---
+++ code_hive/MinimumPossibleSum/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 20240308
+/// https://leetcode.cn/problems/find-the-minimum-possible-sum-of-a-beautiful-array/
+/// </summary>
+public class Solution
+{
+    public int MinimumPossibleSum(int n, int target)
+    {
+        long limit = (target) / 2;
+        if (n <= limit)
+        {
+            return (1 + n) * n / 2 % 1000000007;
+        }
+        else
+        {
+            var sum1 = (1 + limit) * limit / 2;
+            var sum2 = (target + target + n - limit - 1) * (n - limit) / 2;
+            return (int)((sum1 + sum2) % 1000000007);
+        }
+    }
+}
