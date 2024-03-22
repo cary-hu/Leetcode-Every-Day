37c33d2246ddfe0852a4930aabced118e983ae0c
Cary <Cary.hu@devlopertools.com>
2024-03-22 13:50:00 +0800

    20240322

---
+++ code_hive/AddSpaces/Program.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// 20240322
+/// https://leetcode.cn/problems/adding-spaces-to-a-string/
+/// </summary>
+public class Solution
+{
+    public string AddSpaces(string s, int[] spaces)
+    {
+        var chars = new char[s.Length + spaces.Length];
+        for (int i = 0, j = 0; i < s.Length; i++)
+        {
+            if (j >= spaces.Length)
+            {
+                chars[i + j] = s[i];
+                continue;
+            }
+            var currentSpace = spaces[j];
+            if (currentSpace == i)
+            {
+                chars[i + j] = ' ';
+                j++;
+            }
+            chars[i + j] = s[i];
+        }
+        return new string(chars);
+    }
+}
