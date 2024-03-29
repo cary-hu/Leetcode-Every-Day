04f6f67a4b78f4f1cbaa605e3ad35e1ac6896eca
Cary <Cary.hu@devlopertools.com>
2024-03-29 13:42:47 +0800

    20240329

---
+++ code_hive/MinimumSum/Program.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// 20240329
+/// https://leetcode.cn/problems/minimum-sum-of-mountain-triplets-i/
+/// </summary>
+public class Solution
+{
+    public int MinimumSum(int[] nums)
+    {
+        int sum = int.MaxValue;
+        int n = nums.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    if (nums[i] < nums[j] && nums[k] < nums[j])
+                    {
+                        sum = Math.Min(sum, nums[i] + nums[j] + nums[k]);
+                    }
+                }
+            }
+        }
+        return sum == int.MaxValue ? -1 : sum;
+    }
+}
