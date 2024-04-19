a2ead7a187efa46949ea46e71fefec86082958ae
cary.hu <cary.hu@developertools.com>
2024-04-19 14:57:32 +0800

    20240419

---
+++ code_hive/FindingUsersActiveMinutes/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20240419
+/// https://leetcode.cn/problems/finding-the-users-active-minutes/
+/// </summary>
+public class Solution
+{
+    public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+    {
+        var res = new int[k];
+        var allUsers = logs.Select(x => x[0]).Distinct();
+        foreach (var user in allUsers)
+        {
+            var allActionMinuteCount = logs.Where(x => x[0] == user).Select(x => x[1]).Distinct().Count();
+
+            if (allActionMinuteCount == 0)
+            {
+                continue;
+            }
+            res[allActionMinuteCount - 1]++;
+
+        }
+
+        return res;
+    }
+}
