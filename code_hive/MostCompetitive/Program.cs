9566a49ee1be7c6d58173bf1709d5f95ba83193c
cary.hu <cary.hu@developertools.com>
2024-05-24 14:17:40 +0800

    20240524

---
+++ code_hive/MostCompetitive/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 20240524
+/// https://leetcode.cn/problems/find-the-most-competitive-subsequence/
+/// </summary>
+public class Solution
+{
+    public int[] MostCompetitive(int[] nums, int k)
+    {
+        var stack = new Stack<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var n = nums[i];
+            if (stack.Count == 0 || stack.Peek() <= n)
+            {
+                stack.Push(n);
+            }
+            else
+            {
+                while (!(stack.Count == 0) && stack.Peek() > n && stack.Count - 1 + nums.Length - i >= k)
+                {
+                    stack.Pop();
+                }
+                stack.Push(n);
+            }
+        }
+        return stack.Reverse().Take(k).ToArray();
+    }
+}
