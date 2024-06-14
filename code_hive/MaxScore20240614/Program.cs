a66bd592c61df7063a7f213c775bd717ad5dc40e
cary.hu <cary.hu@developertools.com>
2024-06-14 13:54:24 +0800

    20240614

---
+++ code_hive/MaxScore20240614/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 20240614
+/// https://leetcode.cn/problems/visit-array-positions-to-maximize-score/
+/// </summary>
+public class Solution
+{
+    public long MaxScore(int[] nums, int x)
+    {
+        long[] dp = [int.MinValue, int.MinValue];
+        dp[nums[0] % 2] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int parity = (int)(nums[i] % 2);
+            long cur = Math.Max(dp[parity] + nums[i], dp[1 - parity] - x + nums[i]);
+            dp[parity] = Math.Max(dp[parity], cur);
+        }
+        return Math.Max(dp[0], dp[1]);
+    }
+}
