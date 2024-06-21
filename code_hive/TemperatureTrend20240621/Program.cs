b81ed3953a762f3f98e492a2723404bdb36e026a
cary.hu <cary.hu@developertools.com>
2024-06-21 13:43:56 +0800

    20240621

---
+++ code_hive/TemperatureTrend20240621/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int TemperatureTrend(int[] temperatureA, int[] temperatureB)
+    {
+        var res = 0;
+        var days = temperatureA.Length;
+        var currentRes = 0;
+        for (int i = 1; i < days; i++)
+        {
+
+            if ((temperatureA[i] < temperatureA[i - 1] && temperatureB[i] < temperatureB[i - 1]) ||
+                (temperatureA[i] > temperatureA[i - 1] && temperatureB[i] > temperatureB[i - 1]) ||
+                temperatureA[i] == temperatureA[i - 1] && temperatureB[i] == temperatureB[i - 1])
+            {
+                currentRes++;
+                res = Math.Max(res, currentRes);
+            }
+            else
+            {
+                currentRes = 0;
+            }
+        }
+
+        return res;
+    }
+}
