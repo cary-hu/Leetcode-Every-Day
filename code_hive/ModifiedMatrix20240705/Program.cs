952611e467270460b242869f0fdd768b7fb43ed1
cary.hu <cary.hu@developertools.com>
2024-07-05 13:48:54 +0800

    20240705

---
+++ code_hive/ModifiedMatrix20240705/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 20240705
+/// https://leetcode.cn/problems/modify-the-matrix/
+/// </summary>
+public class Solution
+{
+    public int[][] ModifiedMatrix(int[][] matrix)
+    {
+        int n = matrix.Length;
+        int m = matrix[0].Length;
+        for (int j = 0; j < m; j++)
+        {
+            int max = -1;
+            for (int i = 0; i < n; i++)
+            {
+                max = Math.Max(max, matrix[i][j]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (matrix[i][j] == -1)
+                {
+                    matrix[i][j] = max;
+                }
+            }
+        }
+        return matrix;
+    }
+}
---
+++ code_hive/PaintWalls20240628/Program.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// 20240628
+/// https://leetcode.cn/problems/painting-the-walls/description/
+/// </summary>
+/// 
+new Solution().PaintWalls([49, 35, 32, 20, 30, 12, 42], [1, 1, 2, 2, 1, 1, 2]);
+public class Solution
+{
+    public int PaintWalls(int[] cost, int[] time)
+    {
+        var maxCostTime = Math.Ceiling(time.Sum() / 2.0);
+        var x = new Dictionary<int, (int, int)>();
+        for (int i = 0; i < cost.Length; i++)
+        {
+            x.Add(i, (cost[i], time[i]));
+        }
+        var x2 = x.OrderBy(x => x.Value.Item2).OrderBy(x => x.Value.Item1);
+        var costSum = 0;
+        var timeSum = 0;
+        for (int i = 0; i < x2.Count(); i++)
+        {
+            var x3 = x2.ElementAt(i);
+            costSum += x3.Value.Item1;
+            timeSum += x3.Value.Item2;
+            if (timeSum >= maxCostTime || timeSum >= x2.Count() - i)
+            {
+                break;
+            }
+
+        }
+
+        return costSum;
+    }
+}
