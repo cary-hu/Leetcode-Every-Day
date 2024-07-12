4d28691694366fa93fc8b76dc6e7a6f3515f0186
cary.hu <cary.hu@developertools.com>
2024-07-12 13:36:45 +0800

    20240712

---
+++ code_hive/NumberGame20240712/Program.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 20240712
+/// https://leetcode.cn/problems/minimum-number-game/
+/// </summary>
+public class Solution
+{
+    public int[] NumberGame(int[] nums)
+    {
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+        }
+        return nums;
+    }
+}
