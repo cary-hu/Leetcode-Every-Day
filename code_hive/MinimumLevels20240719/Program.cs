b6e5142f0722c059b00b11cccc3de0de82486294
cary.hu <cary.hu@developertools.com>
2024-07-19 13:42:41 +0800

    20240719

---
+++ code_hive/MinimumLevels20240719/Program.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// 20240719
+/// https://leetcode.cn/problems/minimum-levels-to-gain-more-points/
+/// </summary>
+public class Solution
+{
+    public int MinimumLevels(int[] possible)
+    {
+        for (int i = 0; i < possible.Length; i++)
+        {
+            if (possible[i] == 0)
+            {
+                possible[i] = -1;
+            }
+        }
+        var sum = possible.Sum();
+        var prefixSum = 0;
+        var res = -1;
+        for (int i = 1; i < possible.Length; i++)
+        {
+            prefixSum += possible[i - 1];
+            if (prefixSum > (sum - prefixSum))
+            {
+                res = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+}
