37c96e79992dc0c169c9edc3e7006815402b3233
cary.hu <cary.hu@developertools.com>
2024-08-02 13:59:45 +0800

    20240802

---
+++ code_hive/NumberOfRightTriangles20240802/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+/// <summary>
+/// 20240802
+/// https://leetcode.cn/problems/right-triangles/
+/// </summary>
+public class Solution
+{
+    public long NumberOfRightTriangles(int[][] grid)
+    {
+        int n = grid.Length;
+        int m = grid[0].Length;
+        int[] row = new int[n];
+        int[] col = new int[m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        long ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 1 && (row[i] > 1 && col[j] > 1))
+                {
+                    ans += (row[i] - 1) * (col[j] - 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
