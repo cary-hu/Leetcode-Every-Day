59bff595519b9ee44e5aad345574cdb1369b01d7
cary.hu <cary.hu@developertools.com>
2024-08-30 14:13:45 +0800

    20240830

---
+++ code_hive/SumDigitDifferences20240830/Program.cs
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// 20240830
+/// https://leetcode.cn/problems/sum-of-digit-differences-of-all-pairs/
+/// </summary>
+public class Solution
+{
+    public long SumDigitDifferences(int[] nums)
+    {
+        var numStrings = nums.Select(i => i.ToString()).ToList();
+        var dict = new Dictionary<int, long[]>();
+        foreach (var numString in numStrings)
+        {
+            for (int i = 0; i < numString.Length; i++)
+            {
+                var numPart = int.Parse(numString[i].ToString());
+                if (dict.TryGetValue(i, out var value))
+                {
+                    dict[i][numPart] = ++value[numPart];
+                }
+                else
+                {
+                    var ints = new long[10];
+                    Array.Fill(ints, 0);
+                    ints[numPart]++;
+                    dict[i] = ints;
+                }
+
+            }
+        }
+        long sum = 0;
+        var n = nums.Length;
+        foreach (var dictItem in dict)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var c = dictItem.Value[i];
+                sum += c * (n - c) * 1l;
+            }
+        }
+        if (sum < 0)
+        {
+            return 0;
+        }
+        return sum / 2;
+    }
+}
