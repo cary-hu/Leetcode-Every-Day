cac9a2cbacdf8b4248106a013d4a190dc0eff35d
cary.hu <cary.hu@developertools.com>
2024-09-20 14:28:05 +0800

    20240920

---
+++ code_hive/CountSpecialNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// 20240920
+/// https://leetcode.cn/problems/count-special-integers/
+/// </summary>
+/// 
+new Solution().CountSpecialNumbers(20);
+public class Solution
+{
+    public int Res = 0;
+    public int CountSpecialNumbers(int n)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            CheckNumber(i, 1 << i, n);
+        }
+        return Res;
+    }
+
+    public void CheckNumber(long s, int used, int n)
+    {
+        if (s > n)
+        {
+            return;
+        }
+        Res++;
+        for (int i = 0; i <= 9; i++)
+        {
+            if ((used & (1 << i)) == 0)
+            {
+                CheckNumber(s * 10 + i, used | (1 << i), n);
+            }
+        }
+    }
+}
---
+++ code_hive/MaximumRobots20240913/Program.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// 20240913
+/// https://leetcode.cn/problems/maximum-number-of-robots-within-budget/
+/// </summary>
+public class Solution
+{
+    public int MaximumRobots(int[] chargeTimes, int[] runningCosts, long budget)
+    {
+        int res = 0;
+        int n = chargeTimes.Length;
+        long runningCostSum = 0;
+        LinkedList<int> q = new();
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            runningCostSum += runningCosts[i];
+            while (q.Count > 0 && chargeTimes[q.Last.Value] <= chargeTimes[i])
+            {
+                q.RemoveLast();
+            }
+            q.AddLast(i);
+            while (j <= i && (i - j + 1) * runningCostSum + chargeTimes[q.First.Value] > budget)
+            {
+                if (q.Count > 0 && q.First.Value == j)
+                {
+                    q.RemoveFirst();
+                }
+                runningCostSum -= runningCosts[j];
+                j++;
+            }
+            res = Math.Max(res, i - j + 1);
+        }
+        return res;
+    }
+}
