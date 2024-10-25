e94b423fc8178ce7e7d2735cdcbbfeda8af44168
cary.hu <cary.hu@developertools.com>
2024-10-25 14:15:55 +0800

    20241025

---
+++ code_hive/MaxTotalReward20241025/Program.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// 20241025
+/// https://leetcode.cn/problems/maximum-total-reward-using-operations-i/
+/// </summary>
+public class Solution
+{
+    public int MaxTotalReward(int[] rewardValues)
+    {
+        Array.Sort(rewardValues);
+
+        var res = new bool[4010];
+        Array.Fill(res, false);
+        res[0] = true;
+
+        foreach (var reword in rewardValues)
+        {
+            for (int j = reword; j < 2 * reword; j++)
+            {
+                res[j] = res[j] || res[j - reword];
+            }
+        }
+        var max = rewardValues.Max() * 2;
+        for (int i = max - 1; i > 0; i--)
+        {
+            if (res[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
