8ac84e0080823932fdf9ea5612eeb89fb9ab9ae1
cary.hu <cary.hu@developertools.com>
2024-11-22 17:50:31 +0800

    20241122

---
+++ code_hive/MinFlips220241115/Program.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// 20241115
+/// https://leetcode.cn/problems/minimum-number-of-flips-to-make-binary-grid-palindromic-ii/
+/// </summary>
+public class Solution
+{
+    public int MinFlips(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+        var res = 0;
+
+    }
+}
---
+++ code_hive/NonSpecialCount20241122/Program.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// 20241122
+/// https://leetcode.cn/problems/find-the-count-of-numbers-which-are-not-special/
+/// </summary>
+/// 
+new Solution().NonSpecialCount(5, 7);
+public class Solution
+{
+    public int NonSpecialCount(int l, int r)
+    {
+        var count = r - l + 1;
+        int lSqrt = (int)Math.Floor(Math.Sqrt(l));
+        int rSqrt = (int)Math.Floor(Math.Sqrt(r));
+        for (int i = lSqrt; i <= rSqrt; i++)
+        {
+            var n = i * i;
+            if (n >= l && n <= r)
+            {
+                if (IsPrime(i))
+                {
+                    count--;
+                }
+            }
+        }
+        return count;
+    }
+    private bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= boundary; i += 2)
+            if (number % i == 0)
+                return false;
+
+        return true;
+    }
+}
