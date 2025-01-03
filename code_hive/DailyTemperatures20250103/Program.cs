fe12c499859b21898caaadce2f6e2c187b277659
cary.hu <cary.hu@developertools.com>
2025-01-03 13:36:43 +0800

    20250103

---
+++ code_hive/DailyTemperatures20250103/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 20250103
+/// https://leetcode.cn/problems/daily-temperatures/
+/// </summary>
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var res = new int[temperatures.Length];
+        Array.Fill(res, 0);
+        for (int j = 0; j < temperatures.Length; j++)
+        {
+            var current = temperatures[j];
+            for (var i = j + 1; i < temperatures.Length; i++)
+            {
+                if (temperatures[i] > current)
+                {
+                    res[j] = i - j;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
