db346a010fe0d94b59476ff0b83cbcd1f8038fa6
cary.hu <cary.hu@developertools.com>
2025-03-07 13:51:22 +0800

    20250307

---
+++ code_hive/BeautifulSubsets20250307/Program.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// 20250307
+/// https://leetcode.cn/problems/the-number-of-beautiful-subsets/
+/// </summary>
+public class Solution
+{
+    private int Res = 0;
+    public int BeautifulSubsets(int[] nums, int k)
+    {
+        dfs(nums, 0, k, []);
+        return Res;
+    }
+    private void dfs(int[] nums, int idx, int k, List<int> currents)
+    {
+        if (idx == nums.Length)
+        {
+            if (currents.Count != 0)
+            {
+                Res++;
+            }
+            return;
+        }
+        int num = nums[idx];
+        if (!currents.Contains(num + k) && !currents.Contains(num - k))
+        {
+            currents.Add(num);
+            dfs(nums, idx + 1, k, currents);
+            currents.RemoveAt(currents.Count - 1);
+        }
+        dfs(nums, idx + 1, k, currents);
+    }
+}
