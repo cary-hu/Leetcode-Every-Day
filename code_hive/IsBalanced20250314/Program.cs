255c89166299d66c1a5cb82689b2210e91aee0b3
cary.hu <cary.hu@developertools.com>
2025-03-14 13:40:43 +0800

    20250314

---
+++ code_hive/IsBalanced20250314/Program.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// 20250314
+/// https://leetcode.cn/problems/check-balanced-string/
+/// </summary>
+public class Solution
+{
+    public bool IsBalanced(string num)
+    {
+        var chars = num.ToCharArray();
+        var left = 0;
+        var right = 0;
+        var index = 0;
+        foreach (var item in chars)
+        {
+            int x = item - '0';
+            if (index++ % 2 == 0)
+            {
+                left += x;
+            }
+            else
+            {
+                right += x;
+            }
+        }
+        return left == right;
+    }
+}
