9d0aecbd2154590b61bcddaf01c7dc70779aba7a
cary.hu <cary.hu@developertools.com>
2025-03-28 13:38:11 +0800

    20250328

---
+++ code_hive/MaximumOr20250321/Program.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// 20250321
+/// https://leetcode.cn/problems/maximum-or/
+/// </summary>
+public class Solution
+{
+    public long MaximumOr(int[] nums, int k)
+    {
+        int n = nums.Length;
+        if (n == 0) return 0;
+
+        long[] prefix = new long[n];
+        long[] suffix = new long[n];
+
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i - 1] | nums[i];
+        }
+
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            suffix[i] = suffix[i + 1] | nums[i];
+        }
+
+        long maxRes = 0L;
+        for (int i = 0; i < n; i++)
+        {
+            long left = (i > 0) ? prefix[i - 1] : 0L;
+            long right = (i < n - 1) ? suffix[i + 1] : 0L;
+            long shifted = (long)nums[i] << k;
+            long total = left | right | shifted;
+            maxRes = Math.Max(maxRes, total);
+        }
+
+        return maxRes;
+    }
+}
---
+++ code_hive/MinimizedStringLength20250328/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 20250328
+/// https://leetcode.cn/problems/minimize-string-length/
+/// </summary>
+public class Solution
+{
+    public int MinimizedStringLength(string s)
+    {
+        return new HashSet<char>(s.ToCharArray()).Count;
+    }
+}
