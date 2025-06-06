a201fcef23c4c73485c0a1dba8c33235e49c18af
cary.hu <cary.hu@developertools.com>
2025-06-06 13:51:21 +0800

    20250606

---
+++ code_hive/RobotWithString20250606/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+/// <summary>
+/// 20250606
+/// https://leetcode.cn/problems/using-a-robot-to-print-the-lexicographically-smallest-string/
+/// </summary>
+public class Solution
+{
+    public string RobotWithString(string s)
+    {
+        int[] cnt = new int[26];
+        foreach (char c in s)
+        {
+            cnt[c - 'a']++;
+        }
+
+        Stack<char> stack = new Stack<char>();
+        StringBuilder res = new StringBuilder();
+        char minCharacter = 'a';
+        foreach (char c in s)
+        {
+            stack.Push(c);
+            cnt[c - 'a']--;
+            while (minCharacter != 'z' && cnt[minCharacter - 'a'] == 0)
+            {
+                minCharacter++;
+            }
+            while (stack.Count > 0 && stack.Peek() <= minCharacter)
+            {
+                res.Append(stack.Pop());
+            }
+        }
+
+        return res.ToString();
+    }
+}
