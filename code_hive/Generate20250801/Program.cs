0f346b8cfcb0dbc87d90082615c417ae123264a6
cary hu <cary.hu@developertools.com>
2025-08-01 13:33:43 +0800

    20250801

---
+++ code_hive/Generate20250801/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 20250801
+/// https://leetcode.cn/problems/pascals-triangle/
+/// </summary>
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var res = new List<IList<int>>();
+        for (int i = 0; i < numRows; i++)
+        {
+            var row = new List<int>(i + 1);
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    row.Add(1);
+                }
+                else
+                {
+                    row.Add(res[i - 1][j - 1] + res[i - 1][j]);
+                }
+            }
+            res.Add(row);
+        }
+        return res;
+    }
+}
