be9804abcf4ad0921c7541de85b0b8a29ae81206
cary hu <cary.hu@developertools.com>
2025-08-29 13:41:22 +0800

    20250829

---
+++ code_hive/FlowerGame20250829/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// 20250829
+/// https://leetcode.cn/problems/alice-and-bob-playing-flower-game/
+/// </summary>
+public class Solution
+{
+    public long FlowerGame(int n, int m)
+    {
+        var res = 0L;
+        for (int x = 0; x < n; x++)
+        {
+            for (int y = 0; y < m; y++)
+            {
+                if (
+                    (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0)
+                    )
+                {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
