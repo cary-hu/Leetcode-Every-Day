8fc9702531aad65051e80bf38451878e6a75d261
cary hu <cary.hu@developertools.com>
2025-09-05 13:48:47 +0800

    20250905

---
+++ code_hive/MakeTheIntegerZero20250905/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 20250905
+/// https://leetcode.cn/problems/minimum-operations-to-make-the-integer-zero/
+/// </summary>
+public class Solution
+{
+    public int MakeTheIntegerZero(int num1, int num2)
+    {
+        for (long i = 1; i < 61; i++)
+        {
+            var remainNum = num1 - num2 * i;
+            if (remainNum < 0) continue;
+            var bitCount = Convert.ToString(remainNum, 2).Count(c => c == '1');
+            if (bitCount <= i && remainNum >= i) return (int)i;
+
+        }
+        return -1;
+    }
+}
