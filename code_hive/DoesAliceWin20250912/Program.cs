d8d53a304c552926d2736fe71544749c04c23565
cary hu <cary.hu@developertools.com>
2025-09-12 13:41:14 +0800

    20250912

---
+++ code_hive/DoesAliceWin20250912/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 20250912
+/// https://leetcode.cn/problems/vowels-game-in-a-string/description
+/// </summary>
+public class Solution
+{
+    public bool DoesAliceWin(string s)
+    {
+        foreach (var c in s)
+        {
+            if ("aeiou".Contains(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
