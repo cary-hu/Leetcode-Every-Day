bcc32d9d40627464223624d66fa9001da7199537
cary hu <cary.hu@developertools.com>
2025-09-26 13:54:03 +0800

    20250926

---
+++ code_hive/TriangleNumber202250926/Program.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// 20250926
+/// https://leetcode.cn/problems/valid-triangle-number/description/
+/// </summary>
+public class Solution
+{
+    public int TriangleNumber(int[] nums)
+    {
+        Array.Sort(nums);
+        int count = 0;
+        for (int i = nums.Length - 1; i >= 2; i--)
+        {
+            int left = 0, right = i - 1;
+            while (left < right)
+            {
+                if (nums[left] + nums[right] > nums[i])
+                {
+                    count += right - left;
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+}
