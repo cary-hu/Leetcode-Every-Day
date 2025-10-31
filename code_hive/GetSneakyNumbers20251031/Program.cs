edd6e7bb5aecc2372cbfc115c6a49910fc7b6fd7
cary hu <cary.hu@developertools.com>
2025-10-31 13:51:59 +0800

    20251031

---
+++ code_hive/GetSneakyNumbers20251031/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 20251031
+/// https://leetcode.cn/problems/the-two-sneaky-numbers-of-digitville/description/
+/// </summary>
+public class Solution
+{
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        Span<bool> seen = stackalloc bool[nums.Length];
+        Span<int> result = stackalloc int[2];
+        int count = 0;
+        
+        foreach (int num in nums)
+        {
+            if (seen[num])
+            {
+                result[count++] = num;
+                if (count == 2) break;
+            }
+            else
+            {
+                seen[num] = true;
+            }
+        }
+        
+        return result.ToArray();
+    }
+}
