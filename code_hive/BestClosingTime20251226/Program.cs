e92a8182d01fb7694eb02257ad3cb09dec32ec97
cary hu <cary.hu@developertools.com>
2025-12-26 13:56:55 +0800

    20251226

---
+++ code_hive/BestClosingTime20251226/Program.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// 20251226
+/// https://leetcode.cn/problems/minimum-penalty-for-a-shop/
+/// </summary>
+public class Solution
+{
+    public int BestClosingTime(string customers)
+    {
+        int n = customers.Length;
+        int res = int.MaxValue;
+        int currentMinSum = int.MaxValue;
+        int pre = 0;
+        int suf = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            if (currentMinSum > suf + pre)
+            {
+                currentMinSum = suf + pre;
+                res = i;
+            }
+            if (i < n && customers[i] == 'N')
+            {
+                pre++;
+            }
+            else if (i < n)
+            {
+                suf--;
+            }
+        }
+        return res;
+
+    }
+}
