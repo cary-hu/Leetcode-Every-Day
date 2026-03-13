923de4141d9be5d74bfc64a67150d100907da25b
cary hu <cary.hu@developertools.com>
2026-03-13 14:55:13 +0800

    20260313

---
+++ code_hive/MaximizeSquareArea20260116/Program.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// 20260116
+/// https://leetcode.cn/problems/maximum-square-area-by-removing-fences-from-a-field
+/// </summary>
+public class Solution
+{
+    public int MaximizeSquareArea(int m, int n, int[] hFences, int[] vFences)
+    {
+        const int MOD = 1_000_000_007;
+        
+        List<int> hList = [.. hFences, 1, m];
+        List<int> vList = [.. vFences, 1, n];
+        
+        hList.Sort();
+        vList.Sort();
+        
+        HashSet<int> hDistances = [];
+        for (int i = 0; i < hList.Count; i++)
+        {
+            for (int j = i + 1; j < hList.Count; j++)
+            {
+                hDistances.Add(hList[j] - hList[i]);
+            }
+        }
+        
+        int maxSide = -1;
+        for (int i = 0; i < vList.Count; i++)
+        {
+            for (int j = i + 1; j < vList.Count; j++)
+            {
+                int distance = vList[j] - vList[i];
+                if (hDistances.Contains(distance))
+                {
+                    maxSide = Math.Max(maxSide, distance);
+                }
+            }
+        }
+        
+        if (maxSide == -1)
+        {
+            return -1;
+        }
+        
+        long area = (long)maxSide * maxSide % MOD;
+        return (int)area;
+    }
+}
---
+++ code_hive/MinNumberOfSeconds20260313/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Solution
+{
+    public long MinNumberOfSeconds(int mountainHeight, int[] workerTimes)
+    {
+        int maxWorkerTimes = workerTimes.Max();
+
+        long l = 1;
+        long r = (long)maxWorkerTimes * mountainHeight * (mountainHeight + 1) / 2;
+        long ans = 0;
+
+        while (l <= r)
+        {
+            long mid = (l + r) / 2;
+            if (Check(mountainHeight, workerTimes, mid))
+            {
+                ans = mid;
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private bool Check(int mountainHeight, int[] workerTimes, long mid)
+    {
+        long cnt = 0;
+
+        foreach (int t in workerTimes)
+        {
+            long work = mid / t;
+            long k = (long)((-1.0 + Math.Sqrt(1 + work * 8)) / 2);
+            cnt += k;
+        }
+        if (cnt >= mountainHeight)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
