93b588d9f6db2d27d632bb991171cd4fcfeb7f10
caryhu <cary.hu@developertools.com>
2026-07-31 13:38:43 +0800

    20260731

---
+++ code_hive/MinimumPushes20260731/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// 20260731
+/// https://leetcode.cn/problems/minimum-number-of-pushes-to-type-word-ii/
+/// </summary>
+new Solution().MinimumPushes("abcde");
+public class Solution
+{
+    public int MinimumPushes(string word)
+    {
+        var cnt = new int[26];
+        foreach (char b in word)
+        {
+            cnt[b - 'a']++;
+        }
+        Array.Sort(cnt, (a, b) => b.CompareTo(a));
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            ans += cnt[i] * (i / 8 + 1);
+        }
+        return ans;
+
+    }
+}
