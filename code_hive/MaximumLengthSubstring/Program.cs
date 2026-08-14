1b410ff616a5b6b25b323aed28a43770505e3d34
caryhu <cary.hu@developertools.com>
2026-08-14 13:38:44 +0800

    20260814

---
+++ code_hive/MaximumLengthSubstring/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 20260814
+/// https://leetcode.cn/problems/maximum-length-substring-with-two-occurrences/
+/// </summary>
+public class Solution
+{
+    public int MaximumLengthSubstring(string s)
+    {
+
+        int[] count = new int[26];
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            count[s[right] - 'a']++;
+
+            while (count[s[right] - 'a'] > 2)
+            {
+                count[s[left] - 'a']--;
+                left++;
+            }
+
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
